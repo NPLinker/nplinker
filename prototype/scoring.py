82ec7214bc179bd827b5990dee74b1000fd35ed5
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2019-02-25 16:09:44 +0000

    remove old/useless methods, tweak hg_scoring slightly

--- prototype/scoring.py
+++ prototype/scoring.py
@@ -1,8 +1,3 @@
-import multiprocessing
-import time
-import itertools
-
-import psutil
 import numpy as np
 import scipy.stats
 
@@ -12,65 +7,6 @@ import scipy.stats
 # - spectrum only
 # - both
 METCALF_WEIGHTS = [1, 0, -10, 10]
-
-def compute_all_scores_multi(spectra_list, gcf_list, strain_list, scoring_function, do_random=True, cpus=8):
-    spectra_part_list = np.array_split(spectra_list, cpus)
-
-    # check actual number of available CPUs against requested number
-    num_cpus = psutil.cpu_count()
-    if cpus > num_cpus:
-        print('Warning: {} CPUs requested, only {} available'.format(cpus, num_cpus))
-        cpus = num_cpus
-
-    q = multiprocessing.Queue()
-    procs = []
-    for i in range(cpus):
-        if len(spectra_part_list[i]) == 0:
-            continue
-
-        p = multiprocessing.Process(target=compute_all_scores, args=(spectra_part_list[i], gcf_list, strain_list, scoring_function, do_random, q, i))
-        procs.append(p)
-
-    for p in procs:
-        p.start()
-
-    m_scores = {}
-
-    t = time.time()
-    num_finished = 0
-    while num_finished < len(procs):
-        data = q.get()
-        m_scores.update(data)
-        num_finished += 1
-
-    print(('Total time: {:.1f}, {:.1f}/s'.format(time.time() - t, len(spectra_list) / (time.time() - t))))
-    for p in procs:
-        p.join()
-
-    return m_scores
-
-def compute_all_scores(spectra_list, gcf_list, strain_list, scoring_function, do_random=True, q=None, cpu_aff=None):
-    m_scores = {}
-
-    if cpu_aff is not None:
-        psutil.Process().cpu_affinity([cpu_aff])
-
-    for i, spectrum in enumerate(spectra_list):
-        m_scores[spectrum] = {}
-        if i % 100 == 0:
-            print(("Done {} of {}".format(i, len(spectra_list))))
-        for gcf in gcf_list:
-            s, metadata = scoring_function(spectrum, gcf, strain_list)
-            if do_random:
-                s_random, _ = scoring_function(spectrum.random_spectrum, gcf.random_gcf, strain_list)
-            else:
-                s_random = None
-            m_scores[spectrum][gcf] = (s, s_random, metadata)
-
-    if q is not None:
-        q.put(m_scores)
-
-    return m_scores
 
 def metcalf_scoring(spectral_like, gcf_like, strains, both=10, met_not_gcf=-10, gcf_not_met=0, neither=1):
     cum_score = 0
@@ -87,108 +23,8 @@ def metcalf_scoring(spectral_like, gcf_like, strains, both=10, met_not_gcf=-10, 
             cum_score += gcf_not_met
         if not in_gcf and not in_spec:
             cum_score += neither
+
     return cum_score, shared_strains
-
-def compute_all_scores_multi_np(spectra_list, gcf_list, strain_list, scoring_function, do_random=True, cpus=8):
-    spectra_part_list = np.array_split(spectra_list, cpus)
-
-    # check actual number of available CPUs against requested number
-    num_cpus = psutil.cpu_count()
-    if cpus > num_cpus:
-        print('Warning: {} CPUs requested, only {} available'.format(cpus, num_cpus))
-        cpus = num_cpus
-
-    q = multiprocessing.Queue()
-
-    procs = []
-    for i in range(cpus):
-        if len(spectra_part_list[i]) == 0:
-            continue
-
-        p = multiprocessing.Process(target=compute_all_scores_np, args=(spectra_part_list[i], gcf_list, strain_list, scoring_function, do_random, q, i))
-        procs.append(p)
-
-    for p in procs:
-        p.start()
-
-    t = time.time()
-    num_finished = 0
-    m_scores = {}
-
-    # TODO it takes a *lot* of time to send all data back to the main process, almost 50% of 
-    # the total when I was testing. Could probably be significantly reduced by avoiding
-    # the use of Spectrum and GCF objects in m_scores to keep memory usage down and using
-    # indices into an existing set of them held elsewhere.
-    while num_finished < len(procs):
-        data = q.get()
-        m_scores.update(data)
-        num_finished += 1
-
-    for p in procs:
-        p.join()
-
-    print(('Total time: {:.1f} secs, {:.1f} scores/sec'.format(time.time() - t, len(spectra_list) / (time.time() - t))))
-    return m_scores
-
-def compute_all_scores_np(spectra_list, gcf_list, strain_list, scoring_function, do_random=True, q=None, cpu_aff=None):
-    if cpu_aff is not None:
-        psutil.Process().cpu_affinity([cpu_aff])
-
-    if cpu_aff is not None:
-        print('compute_all_scores_np on CPU {}, processing {} spectra'.format(cpu_aff, len(spectra_list)))
-    else:
-        print('compute_all_scores_np processing {} spectra'.format(len(spectra_list)))
-
-    started = time.time()
-    m_scores = {s: {} for s in spectra_list}
-
-    # TODO: this could possibly be made faster by adjusting the datatypes slightly and using 
-    # multiprocessing.Arrays instead, or a shared data structure via multiprocessing.Manager
-    for spectrum, gcf in itertools.product(spectra_list, gcf_list):
-        s, s_random, metadata = scoring_function(spectrum, gcf, strain_list, do_random)
-
-        # don't care about results where there are no common strains, so exclude them here
-        # (this saves a *lot* of entries in m_scores and makes the scoring significantly
-        # faster overall)
-        if metadata is not None:
-            m_scores[spectrum][gcf] = (s, s_random, metadata)
-
-    if q is not None:
-        q.put(m_scores)
-
-    ended = time.time()
-
-    if cpu_aff is not None:
-        print('compute_all_scores_np on CPU {}, total time = {:.1f}s, {:.1f} scores/sec'.format(cpu_aff, ended - started, len(m_scores) / (ended - started)))
-    else:
-        print('compute_all_scores_np: total time = {:.1f}s, {:.1f} scores/sec'.format(ended - started, len(m_scores) / (ended - started)))
-
-    if cpu_aff is None:
-        return m_scores
-
-def metcalf_scoring_np(spectral_like, gcf_like, strains, do_random, weights=METCALF_WEIGHTS):
-    # each strains_lookup array is treated as containing 1s/0s, so multiplying one
-    # of them by 2 and adding together will give a new array containing values in
-    # the range 0-3: 0 = neither, 1 = GCF only, 2 = spectrum only, 3 = both
-    result = (2 * spectral_like.strains_lookup) + gcf_like.strains_lookup
-
-    # use bincount to total up the number of occurrences of each value in result,
-    # then just multiply by the weights and sum to get final score (minlength=4 ensures
-    # that a 4-element array is always returned even if one of the 4 possible values
-    # doesn't appear in result)
-    cum_score = np.sum(np.bincount(result, minlength=4) * weights)
-
-    cum_score_rand = 0
-    if do_random:
-        rand_result = (2 * spectral_like.random_spectrum.strains_lookup) + gcf_like.random_gcf.strains_lookup
-        cum_score_rand = np.sum(np.bincount(rand_result, minlength=4) * weights)
-
-    # get common strains, if any
-    common = np.where(result == 3)[0]
-    if len(common) > 0:
-        return cum_score, cum_score_rand, strains[common]
-
-    return cum_score, cum_score_rand, None
 
 def hg_scoring(spectral_like, gcf_like, strains):
     spectral_count = 0
@@ -196,11 +32,13 @@ def hg_scoring(spectral_like, gcf_like, strains):
     overlap_count = 0
 
     for strain in strains:
-        if spectral_like.has_strain(strain):
+        in_spec = spectral_like.has_strain(strain)
+        in_gcf = gcf_like.has_strain(strain)
+        if in_spec:
             spectral_count += 1
-        if gcf_like.has_strain(strain):
+        if in_gcf:
             gcf_count += 1
-        if spectral_like.has_strain(strain) and gcf_like.has_strain(strain):
+        if in_gcf and in_spec:
             overlap_count += 1
 
     pos_in_sample = overlap_count
@@ -208,6 +46,8 @@ def hg_scoring(spectral_like, gcf_like, strains):
     n = gcf_count
     M = len(strains)
 
+    # sf(k, M, n, N, loc=0)
+    # sf(num_shared_strains, num_strains, strains_in_gcf, strains_in_spec, 1)
     r = scipy.stats.hypergeom.sf(pos_in_sample, M, n, N, 1)
     return r, None
 
