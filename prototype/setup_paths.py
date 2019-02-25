5bd093a79de09e6469d62333bc225543e95cecf5
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2019-02-25 16:04:30 +0000

    updating setup_paths.py
    - remove old method with hardcoded paths
    - improve error handling / warning messages

--- prototype/setup_paths.py
+++ prototype/setup_paths.py
@@ -1,46 +1,33 @@
-import os,glob
+import os
+import glob
+
+def _find_via_glob(path, file_type):
+    try:
+        filename = glob.glob(path)[0]
+        return filename
+    except OSError as ose:
+        raise Exception('{}: unable to find {} in path "{}"'.format(ose, file_type, path))
 
 def setup_paths(root_dir):
-    MIBIG_JSON_DIR = os.path.join(root_dir,'mibig_json')
-    print("MIBIG_JSON_DIR:",MIBIG_JSON_DIR)
+    """Given a root directory for a dataset, attempts to construct the filenames
+        that are necessary to load everything into nplinker.
+    """
 
-    spec_dir = os.path.join(root_dir,'spectra')
-    MGF_FILE = glob.glob(os.path.join(spec_dir,'*.mgf'))[0]
+    # just <root_dir>/mibig_json
+    MIBIG_JSON_DIR = os.path.join(root_dir, 'mibig_json')
 
-    print("MGF_FILE:",MGF_FILE)
-    ANNOTATION_FILES = glob.glob(os.path.join(spec_dir,'*.annotations.*'))
-    print("ANNOTATION_FILES:"," ".join(ANNOTATION_FILES))
+    # should have an MGF file named <root_dir>/spectra/<something>.mgf
+    spec_dir = os.path.join(root_dir, 'spectra')
+    MGF_FILE = _find_via_glob(os.path.join(spec_dir, '*.mgf'), 'MGF file')
 
-    EDGES_FILE = glob.glob(os.path.join(spec_dir,"*.pairsinfo"))[0]
-    print("EDGES_FILE:",EDGES_FILE)
+    # annotation file(s) may not exist
+    ANNOTATION_FILES = glob.glob(os.path.join(spec_dir, '*.annotations.*'))
 
-    NODES_FILE = glob.glob(os.path.join(spec_dir,'*.out'))[0]
-    print("NODES_FILE:",NODES_FILE)
+    # both of these should exist
+    EDGES_FILE = _find_via_glob(os.path.join(spec_dir, "*.pairsinfo"), 'edges file')
+    NODES_FILE = _find_via_glob(os.path.join(spec_dir, '*.out'), 'nodes file')
 
-    ROOT_PATH = os.path.join(root_dir,'bigscape')
-    ANTISMASH_DIR = os.path.join(root_dir,'antismash')
+    ROOT_PATH = os.path.join(root_dir, 'bigscape')
+    ANTISMASH_DIR = os.path.join(root_dir, 'antismash')
 
-    return MIBIG_JSON_DIR,NODES_FILE,ANNOTATION_FILES,MGF_FILE,EDGES_FILE,ROOT_PATH,ANTISMASH_DIR
-
-
-def setup_paths_simon():
-
-
-
-
-    MIBIG_JSON_DIR = "/Users/simon/Desktop/carnegie_bigscape/mibig/mibig_json-1.4/"
-    # PATH_MS2LDA = DATASET
-
-    # annotations
-    NODES_FILE = "/Users/simon/Dropbox/BioResearch/Meta_clustering/MS2LDA/carnegie_gnps_data/ProteoSAFe-METABOLOMICS-SNETS-2cdc2aa3-download_clustered_spectra-2/clusterinfosummarygroup_attributes_withIDs_withcomponentID/reformated_derep.tsv"
-    DEREP_ANNOTATIONS = "/Users/simon/Dropbox/BioResearch/Meta_clustering/MS2LDA/carnegie_gnps_data/cluster_to_derep.tsv"
-
-    MGF_FILE = "/Users/simon/Dropbox/BioResearch/Meta_clustering/MS2LDA/carnegie_gnps_data/ProteoSAFe-METABOLOMICS-SNETS-2cdc2aa3-download_clustered_spectra-2/METABOLOMICS-SNETS-2cdc2aa3-download_clustered_spectra-main.mgf"
-    EDGES_FILE = "/Users/simon/Dropbox/BioResearch/Meta_clustering/MS2LDA/carnegie_gnps_data/ProteoSAFe-METABOLOMICS-SNETS-2cdc2aa3-download_clustered_spectra-2/networkedges_selfloop/1eca77da22c84ae1aee490d0e4bc26f3.pairsinfo"
-
-    FOLDERS = ['NRPS','Others','PKSI','PKS-NRP_Hybrids','PKSother','RiPPs','Saccharides','Terpene']
-    FOLDERS = ['NRPS']
-    # GCF file path
-    ROOT_PATH = "/Users/simon/Desktop/carnegie_bigscape/bigscape-carnegie-glocal/network_files/2018-11-16_12-01-18_hybrids_glocal/"
-    ANTISMASH_DIR = "/Users/simon/Desktop/carnegie_bigscape/antismash/"
-    return MIBIG_JSON_DIR,NODES_FILE,DEREP_ANNOTATIONS,MGF_FILE,EDGES_FILE,FOLDERS,ROOT_PATH,ANTISMASH_DIR
+    return MIBIG_JSON_DIR, NODES_FILE, ANNOTATION_FILES, MGF_FILE, EDGES_FILE, ROOT_PATH, ANTISMASH_DIR
