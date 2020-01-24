4186b95abb222d388daa80aa20cf238b4cfbde88
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2020-01-24 00:35:52 +0000

    add mibig download+extract; add strain extraction and storage in mappings file; tweak file extraction

--- prototype/nplinker/pairedomics/downloader.py
+++ prototype/nplinker/pairedomics/downloader.py
@@ -2,12 +2,15 @@ import sys
 import os
 import zipfile
 import json
+import tarfile
 
 import httpx
 from bs4 import BeautifulSoup
 from xdg import XDG_CONFIG_HOME
 from progress.bar import Bar
 from progress.spinner import Spinner
+
+from ..strains import StrainCollection, Strain
 
 from ..logconfig import LogConfig
 logger = LogConfig.getLogger(__file__)
@@ -22,8 +25,10 @@ class Downloader(object):
     NCBI_GENBANK_LOOKUP_URL = 'https://www.ncbi.nlm.nih.gov/nuccore/{}?report=docsum'
     NCBI_ASSEMBLY_LOOKUP_URL = 'https://www.ncbi.nlm.nih.gov/assembly?LinkName=nuccore_assembly&from_uid={}'
 
-    def __init__(self, project_id, force_download=False):
-        self.gnps_massive_id = project_id
+    MIBIG_JSON_URL = 'https://dl.secondarymetabolites.org/mibig/mibig_json_{}.tar.gz'
+
+    def __init__(self, platform_id, force_download=False):
+        self.gnps_massive_id = platform_id
         self.pairedomics_id = None
         self.gnps_task_id = None
         self.local_cache = os.path.join(os.getenv('HOME'), 'nplinker_data', 'pairedomics')
@@ -34,8 +39,10 @@ class Downloader(object):
         os.makedirs(self.local_cache, exist_ok=True)
 
         self.json_data = None
+        self.strains = StrainCollection()
+        self.growth_media = {}
         
-        logger.info('Downloader for {}, caching to {}'.format(project_id, self.local_cache))
+        logger.info('Downloader for {}, caching to {}'.format(platform_id, self.local_cache))
 
         if not os.path.exists(self.local_project_json_file) or force_download:
             logger.info('Downloading new copy of platform project data...')
@@ -50,7 +57,7 @@ class Downloader(object):
             pairedomics_id = project['_id']
             gnps_massive_id = project['project']['metabolomics']['project']['GNPSMassIVE_ID']
 
-            if gnps_massive_id == project_id:
+            if gnps_massive_id == platform_id:
                 self.pairedomics_id = pairedomics_id
                 if 'molecular_network' not in project['project']['metabolomics']['project']:
                     raise Exception('Dataset has no GNPS data URL!')
@@ -69,15 +76,41 @@ class Downloader(object):
         self.project_file_cache = os.path.join(self.local_file_cache, self.gnps_massive_id)
         os.makedirs(self.project_file_cache, exist_ok=True)
 
+        with open(os.path.join(self.project_file_cache, 'platform_data.json'), 'w') as f:
+            f.write(str(self.project_json))
+
+        self.strain_mappings_file = os.path.join(self.project_file_cache, 'strain_mappings.csv')
+
     def get(self):
         logger.info('Going to download the metabolomics data file')
 
         self._download_metabolomics_zipfile(self.gnps_task_id)
-        self._parse_genome_labels(self.project_json['project']['genome_metabolome_links'])
         self._download_genomics_data(self.project_json['project']['genomes'])
+        self._parse_genome_labels(self.project_json['project']['genome_metabolome_links'], self.project_json['project']['genomes'])
+        self._generate_strain_mappings()
+        self._download_mibig_json() # TODO version
 
     def _is_new_gnps_format(self, directory):
         return os.path.exists(os.path.join(directory, 'qiime2_output'))
+
+    def _generate_strain_mappings(self):
+        # first time downloading, this file will not exist, should only need done once
+        if not os.path.exists(self.strain_mappings_file):
+            logger.info('Generating strain mappings file')
+            for root, dirs, files in os.walk(os.path.join(self.project_file_cache, 'antismash')):
+                for f in files: 
+                    if not f.endswith('.gbk'):
+                        continue
+
+                    # TODO is this correct/sensible???
+                    strain_name = os.path.split(root)[1]
+                    strain_alias = os.path.splitext(f)[0]
+                    strain_alias = strain_alias[:strain_alias.index('.')]
+                    # print('mapping {} => {}'.format(strain_alias, self.strains.lookup(strain_name)))
+                    self.strains.lookup(strain_name).add_alias(strain_alias)
+            self.strains.save_to_file(self.strain_mappings_file)
+        else:
+            logger.info('Strain mappings already generated')
 
     def _download_genomics_data(self, genome_records):
         found = 0
@@ -133,7 +166,7 @@ class Downloader(object):
             for mf in missing_files:
                 f.write('{}\n'.format(mf))
 
-        print('Obtained {}/{} sets of genome data'.format(found, len(genome_records)))
+        logger.info('Obtained {}/{} sets of genome data'.format(found, len(genome_records)))
         if found == 0:
             raise Exception('Failed to download ANY genome data!')
 
@@ -176,7 +209,50 @@ class Downloader(object):
 
         return refseq_id
 
+    def _download_mibig_json(self, version='1.4'):
+        local_path = os.path.join(self.project_download_cache, 'mibig_json_{}.tar.gz'.format(version))
+        logger.debug('Checking for existing MiBIG archive at {}'.format(local_path))
+        cached = False
+        if os.path.exists(local_path):
+            logger.info('Found cached file at {}'.format(local_path))
+            try:
+                agz = tarfile.open(local_path)
+                cached = True
+            except:
+                logger.info('Invalid MiBIG archive found, will download again')
+                os.unlink(local_path)
 
+        if not cached:
+            url = Downloader.MIBIG_JSON_URL.format(version)
+            with open(local_path, 'wb') as f:
+                total_bytes, last_total = 0, 0
+                with httpx.stream('GET', url) as r:
+                    filesize = int(r.headers['content-length'])
+                    bar = Bar(url, max=filesize, suffix='%(percent)d%%')
+                    for data in r.iter_bytes():
+                        f.write(data)
+                        total_bytes += len(data)
+                        bar.next(len(data))
+                    bar.finish()
+        
+        logger.debug('Extracting MiBIG JSON data')
+
+        output_path = os.path.join(self.project_file_cache, 'mibig_json')
+        if os.path.exists(os.path.join(output_path, 'completed')):
+            return True
+
+        mibig_gz = tarfile.open(local_path, 'r:gz')
+        # extract and rename to "mibig_json"
+        # TODO annoyingly the 2.0 version has been archived with a subdirectory, while
+        # 1.4 just dumps all the files into the current directory, so if/when 2.0 support
+        # is required this will need to handle both cases
+        mibig_gz.extractall(path=os.path.join(self.project_file_cache, 'mibig_json'))
+        # os.rename(os.path.join(self.project_file_cache, 'mibig_json_{}'.format(version)), os.path.join(self.project_file_cache, 'mibig_json'))
+
+        open(os.path.join(output_path, 'completed'), 'w').close()
+
+        return True
+    
     def _download_antismash_zip(self, accession_id):
         # save files as <accession>.zip to avoid having to repeat above lookup every time
         local_path = os.path.join(self.project_download_cache, '{}.zip'.format(accession_id))
@@ -217,20 +293,55 @@ class Downloader(object):
         
         logger.debug('Extracting antismash data')
 
+        output_path = os.path.join(self.project_file_cache, 'antismash', os.path.splitext(os.path.split(local_path)[1])[0])
+        if os.path.exists(os.path.join(output_path, 'completed')):
+            logger.debug('antismash data already extracted!')
+            return True
+
         antismash_zip = zipfile.ZipFile(local_path)
         kc_prefix = '{}/knownclusterblast'.format(accession_id)
+        antismash_path = os.path.join(self.project_file_cache, 'antismash')
         for zip_member in antismash_zip.namelist():
             # TODO other files here?
-            if zip_member.endswith('.gbk') or zip_member.startswith(kc_prefix):
-                antismash_zip.extract(zip_member, path=os.path.join(self.project_file_cache, 'antismash'))
+            if zip_member.endswith('.gbk'):
+                antismash_zip.extract(zip_member, path=antismash_path)
+            elif zip_member.startswith(kc_prefix):
+                antismash_zip.extract(zip_member, path=antismash_path)
+
+        open(os.path.join(output_path, 'completed'), 'w').close()
 
         return True
 
-    def _parse_genome_labels(self, genome_metabolome_links):
-        # TODO have to update this after actually parsing metabolomics TSV
-        for gml in genome_metabolome_links:
-            label = gml['genome_label']
-            filename = os.path.split(gml['metabolomics_file'])[1]
+    def _parse_genome_labels(self, met_records, gen_records):
+        temp = {}
+        mc, gc = 0, 0
+
+        for rec in met_records:
+            label = rec['genome_label']
+            filename = os.path.split(rec['metabolomics_file'])[1]
+            temp[label] = Strain(label)
+            temp[label].add_alias(filename)
+            mc += 1
+
+            if label in self.growth_media:
+                self.growth_media[label].add(rec['sample_preparation_label'])
+            else:
+                self.growth_media[label] = set([rec['sample_preparation_label']])
+
+        for rec in gen_records:
+            label = rec['genome_label']
+            # TODO other accessions?
+            accession = rec['genome_ID']['RefSeq_accession']
+            accession = accession[:accession.rindex('.')]
+            if label in temp:
+                temp[label].add_alias(accession)
+            else:
+                temp[label] = Strain(label)
+                gc += 1
+
+        logger.info('Extracted {} strains from JSON (met={}, gen={})'.format(len(temp), mc, gc))
+        for strain in temp.values():
+            self.strains.add(strain)
 
     def _download_metabolomics_zipfile(self, gnps_task_id):
         url = Downloader.GNPS_DATA_DOWNLOAD_URL.format(gnps_task_id)
@@ -272,10 +383,14 @@ class Downloader(object):
         # - root/clusterinfosummarygroup_attributes_withIDs_withcomponentID/*.tsv
         # - root/networkedges_selfloop/*.pairsinfo
         for member in mbzip.namelist():
-            if member.endswith('.mgf')\
-                or member.startswith('clusterinfosummarygroup_attributes_withIDs_withcomponentID')\
+            if member.startswith('clusterinfosummarygroup_attributes_withIDs_withcomponentID')\
                 or member.startswith('networkedges_selfloop'):
                     mbzip.extract(member, path=self.project_file_cache)
+            # move the MGF file to a /spectra subdirectory to better fit expected structure
+            elif member.endswith('.mgf'):
+                os.makedirs(os.path.join(self.project_file_cache, 'spectra'), exist_ok=True)
+                mbzip.extract(member, path=os.path.join(self.project_file_cache, 'spectra'))
+                        
         if self._is_new_gnps_format(self.project_file_cache):
             logger.info('Found NEW GNPS structure')
         else:
