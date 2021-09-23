e5405d4ffaa9adc7019edfd5685c7cd17fe6e2ba
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2021-09-23 15:55:01 +0100

    Updates to handling of BiG-SCAPE parameters
     - Update default configuration file to describe new defaults and emphasize inclusion of "--mibig"
     - Update description of other BiG-SCAPE parameters
     - Update loader module with the new defaults
     - Remove custom handling of MiBIG parameters in run_bigscape.py
     - Remove hardcoded inclusion of "--clans-off" in run_bigscape.py
     - Remove semi-separate handling of "--cutoffs" default value, it's now set like other parameters
     - Check BiG-SCAPE process return code to avoid NPLinker incorrectly treating it as having completed successfully

--- prototype/nplinker/pairedomics/runbigscape.py
+++ prototype/nplinker/pairedomics/runbigscape.py
@@ -23,8 +23,8 @@ logger = LogConfig.getLogger(__file__)
 # NOTE: for simplicity this is currently written with assumption it will only be
 # called in context of nplinker Docker image, where bigscape should be available
 
-def run_bigscape(bigscape_py_path, antismash_path, output_path, pfam_path, cutoffs=[0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7], mibig=True, mibig_14=False, mibig_13=False, extra_params=None):
-    logger.info('run_bigscape: input="{}", output="{}", cutoffs={}"'.format(antismash_path, output_path, cutoffs))
+def run_bigscape(bigscape_py_path, antismash_path, output_path, pfam_path, extra_params):
+    logger.info('run_bigscape: input="{}", output="{}", extra_params={}"'.format(antismash_path, output_path, extra_params))
 
     if os.path.exists(os.path.join(output_path, 'completed')):
         logger.info('BiG-SCAPE appears to have been run already, skipping!')
@@ -39,37 +39,20 @@ def run_bigscape(bigscape_py_path, antismash_path, output_path, pfam_path, cutof
     if not os.path.exists(antismash_path):
         raise Exception('antismash_path "{}" does not exist!'.format(antismash_path))
 
-    if cutoffs is None or len(cutoffs) == 0:
-        raise Exception('Should provide at least one cutoff value!')
-
-    if mibig_14 and mibig_13:
-        raise Exception('Only one of mibig_14 and mibig_13 should be set!')
-
     # configure the IO-related parameters, including pfam_dir
     args = [bigscape_py_path, '-i', antismash_path, '-o', output_path, '--pfam_dir', pfam_path]
 
-    # use default cutoffs if not overridden by user
-    if extra_params is not None and extra_params.find('cutoffs') == -1:
-        args.append('--cutoffs')
-        args.append(','.join(map(str, cutoffs)))
-
     # append the user supplied params, if any
-    if extra_params is not None and len(extra_params) > 0:
-        args.append(extra_params)
+    if len(extra_params) > 0:
+        args.extend(extra_params.split(' '))
 
-    if mibig_13:
-        args.append('--mibig13')
-    elif mibig_14:
-        args.append('--mibig14')
-    elif mibig:
-        args.append('--mibig') # default
-
-    # disable extra clustering (TODO OK?)
-    args.append('--clans-off')
-
-    logger.info('bigscape command: {}'.format(args))
+    logger.info('BiG-SCAPE command: {}'.format(args))
     result = subprocess.run(args, stdout=sys.stdout, stderr=sys.stderr)
-    logger.info('bigscape completed!')
+    logger.info('BiG-SCAPE completed with return code {}'.format(result.returncode))
+    # use subprocess.CompletedProcess.check_returncode() to test if the BiG-SCAPE
+    # process exited successfully. This throws an exception for non-zero returncodes
+    # which will indicate to the Downloader module that something went wrong.
+    result.check_returncode()
 
     # use presence of this file as a quick way to check if a previous run
     # finished or not 
