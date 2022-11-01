a0c0ae8736811b17911ef972c171006a731f773b
Cunliang Geng <c.geng@esciencecenter.nl>
2022-11-01 16:24:08 +0100

    Update __init__.py

--- src/nplinker/genomics/__init__.py
+++ src/nplinker/genomics/__init__.py
@@ -7,15 +7,10 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 from .bgc import BGC
 from .gcf import GCF
 from .genomics import loadBGC_from_cluster_files
-from .genomics import make_mibig_bgc_dict
-from .mibigbgc import MibigBGC
 
 
 __all__ = [
     "bgc",
-    "mibig",
-    "mibigbgc",
     "gcf",
     "loadBGC_from_cluster_files",
-    "make_mibig_bgc_dict"
 ]
