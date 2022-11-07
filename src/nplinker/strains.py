5603189f95466c55c444860ad11ad6e6d10b3747
Cunliang Geng <c.geng@esciencecenter.nl>
2022-11-07 13:28:32 +0100

    add docstring and static typing and rename parameters

--- src/nplinker/strains.py
+++ src/nplinker/strains.py
@@ -1,20 +1,4 @@
-# Copyright 2021 The NPLinker Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 from .logconfig import LogConfig
-
 
 logger = LogConfig.getLogger(__file__)
 
@@ -25,16 +9,29 @@ class Strain():
         self.id = primary_strain_id
         self.aliases = set()
 
-    def has_alias(self, alt_id):
-        return alt_id in self.aliases
+    def has_alias(self, name: str) -> bool:
+        """Check if alias name exist
 
-    def add_alias(self, alt_id):
-        if len(alt_id) == 0:
+        Args:
+            name(str): strain alias name
+
+        Returns:
+            bool: if given name exist return True
+        """
+        return name in self.aliases
+
+    def add_alias(self, name: str) -> None:
+        """Add one alias name to aliases set
+
+        Args:
+            name(str): strain alias name
+        """
+        if len(name) == 0:
             logger.warning(
                 f'Refusing to add zero-length alias to strain {self}')
             return
 
-        self.aliases.add(alt_id)
+        self.aliases.add(name)
 
     def __repr__(self):
         return str(self)
