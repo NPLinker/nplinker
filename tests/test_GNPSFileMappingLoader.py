56f2e55354a6832aa27371c74e913e778bd816db
hechth <helge.hecht@recetox.muni.cz>
2022-11-10 15:47:21 +0100

    harmonized mapping test

--- tests/test_GNPSFileMappingLoader.py
+++ tests/test_GNPSFileMappingLoader.py
@@ -11,29 +11,15 @@ def loader() -> GNPSFileMappingLoader:
 def test_default(loader):
     assert loader is not None
 
-
-@pytest.mark.parametrize("filename, expected_length, spectrum_id", [
-    [ DATA_DIR / "nodes_mwe.csv", 13, 275],
-    [ DATA_DIR / "nodes.tsv", 25935, 223]
-])
-def test_load_mapping_allfiles(filename, expected_length, spectrum_id):
-    sut = GNPSFileMappingLoader(filename)
-    sut.load_mapping_allfiles()
-
-    actual = sut.mapping()
-
-    assert len(actual) == expected_length
-    assert actual[spectrum_id] == ["26c.mzXML", "26c.mzXML", "26c.mzXML"]
-
-
 @pytest.mark.parametrize("filename, expected_length, spectrum_id, samples", [
     [DATA_DIR / "nodes_fbmn_mwe.csv", 9, 301, ["20210623_10_9A_1uL.mzML", "20210623_16_9C_1uL.mzML", "20210623_12_5B_1uL.mzML", "20210623_13_9B_1uL.mzML"]],
     [DATA_DIR / "nodes_fbmn_mwe.csv", 9, 1465, ["20210623_10_9A_1uL.mzML", "20210623_16_9C_1uL.mzML"]],
-    [DATA_DIR / "nodes_fbmn.csv", 994, 304, ["20210623_10_9A_1uL.mzML", "20210623_16_9C_1uL.mzML", "20210623_13_9B_1uL.mzML"]]
+    [DATA_DIR / "nodes_fbmn.csv", 994, 304, ["20210623_10_9A_1uL.mzML", "20210623_16_9C_1uL.mzML", "20210623_13_9B_1uL.mzML"]],
+    [ DATA_DIR / "nodes_mwe.csv", 13, 275, ["26c.mzXML", "26c.mzXML", "26c.mzXML"]],
+    [ DATA_DIR / "nodes.tsv", 25935, 223, ["26c.mzXML", "26c.mzXML", "26c.mzXML"]]
 ])
-def test_load_mapping_fbmn(filename, expected_length, spectrum_id, samples):
+def test_load_mapping(filename, expected_length, spectrum_id, samples):
     sut = GNPSFileMappingLoader(filename)
-    sut.load_mapping_fbmn()
     actual = sut.mapping()
 
     assert actual[spectrum_id] == samples
