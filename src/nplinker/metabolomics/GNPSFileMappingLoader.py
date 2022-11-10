0f4019436d7f913c3b23d058e545359308c5ab60
hechth <helge.hecht@recetox.muni.cz>
2022-11-10 15:14:38 +0100

    Implemented fbmn format reader

--- src/nplinker/metabolomics/GNPSFileMappingLoader.py
+++ src/nplinker/metabolomics/GNPSFileMappingLoader.py
@@ -1,38 +1,60 @@
 import csv
 from typing import Dict, Literal
+from nplinker.logconfig import LogConfig
 from nplinker.metabolomics.IFileMappingLoader import IFileMappingLoader
 from nplinker.metabolomics.load_gnps import identify_gnps_format
 from nplinker.utils import find_delimiter
 
+logger = LogConfig.getLogger(__file__)
+
+FILE_IDENTIFIER_FBMN = " Peak area"
 
 class GNPSFileMappingLoader(IFileMappingLoader):
 
     def __init__(self, filename: str):
         self._filename: str = filename
-        self._mapping: Dict[int, str] = {}
+        self._mapping: Dict[int, list[str]] = {}
 
-    def mapping(self):
+    def mapping(self) -> Dict[int, list[str]]:
         return self._mapping
 
     def load_mapping_allfiles(self):
         with open(self._filename, mode='rt', encoding='utf-8') as file:
 
-            delimiter = find_delimiter(self._filename)
-            reader = csv.reader(file, delimiter=delimiter)
-            header: list[str] = next(reader)
-            reader = csv.DictReader(file, header, delimiter=delimiter)
+            reader = self.dict_reader(file)
  
             for row in reader:
                 spectrum_id = int(row["cluster index"])
+                
                 occurrences = row["AllFiles"].split("###") # split by '###'
                 occurrences.pop() # remove last empty entry
-
                 # separate the scan position from the files
                 samples = [x.split(':')[0] for x in occurrences]
 
                 self._mapping[spectrum_id] = samples
 
+    def dict_reader(self, file):
+        delimiter = find_delimiter(self._filename)
+        reader = csv.reader(file, delimiter=delimiter)
+        header: list[str] = next(reader)
+        reader = csv.DictReader(file, header, delimiter=delimiter)
+        return reader
+
     def load_mapping_fbmn(self):
         with open(self._filename, mode='rt', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            line: list[str] = next(reader)
+            reader = self.dict_reader(file)
+
+            for row in reader:
+                spectrum_id = int(row["row ID"])
+
+                if self._mapping.get(spectrum_id) is not None:
+                    logger.warning("Found duplicated row ID: %{spectrum_id}")
+
+                for col in row:
+                    if FILE_IDENTIFIER_FBMN in col:
+                        if float(row[col]) > 0:
+                            sample = col.strip(FILE_IDENTIFIER_FBMN)
+                            if self._mapping.get(spectrum_id) is None:
+                                self._mapping[spectrum_id] = [sample]
+                            else:
+                                self._mapping[spectrum_id].append(sample)
--- tests/test_GNPSFileMappingLoader.py
+++ tests/test_GNPSFileMappingLoader.py
@@ -24,3 +24,17 @@ def test_load_mapping_allfiles(filename, expected_length, spectrum_id):
 
     assert len(actual) == expected_length
     assert actual[spectrum_id] == ["26c.mzXML", "26c.mzXML", "26c.mzXML"]
+
+
+@pytest.mark.parametrize("filename, expected_length, spectrum_id, samples", [
+    [DATA_DIR / "nodes_fbmn_mwe.csv", 9, 301, ["20210623_10_9A_1uL.mzML", "20210623_16_9C_1uL.mzML", "20210623_12_5B_1uL.mzML", "20210623_13_9B_1uL.mzML"]],
+    [DATA_DIR / "nodes_fbmn_mwe.csv", 9, 1465, ["20210623_10_9A_1uL.mzML", "20210623_16_9C_1uL.mzML"]],
+    [DATA_DIR / "nodes_fbmn.csv", 994, 304, ["20210623_10_9A_1uL.mzML", "20210623_16_9C_1uL.mzML", "20210623_13_9B_1uL.mzML"]]
+])
+def test_load_mapping_fbmn(filename, expected_length, spectrum_id, samples):
+    sut = GNPSFileMappingLoader(filename)
+    sut.load_mapping_fbmn()
+    actual = sut.mapping()
+
+    assert actual[spectrum_id] == samples
+    assert len(actual) == expected_length
