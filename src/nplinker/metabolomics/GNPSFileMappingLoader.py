ac84e67c068d41fcba01ed6a38edba35b4dbffdc
hechth <helge.hecht@recetox.muni.cz>
2022-11-10 13:35:22 +0100

    Added some functions for GNPSFileMappingLoader

--- src/nplinker/metabolomics/GNPSFileMappingLoader.py
+++ src/nplinker/metabolomics/GNPSFileMappingLoader.py
@@ -2,21 +2,37 @@ import csv
 from typing import Dict, Literal
 from nplinker.metabolomics.IFileMappingLoader import IFileMappingLoader
 from nplinker.metabolomics.load_gnps import identify_gnps_format
+from nplinker.utils import find_delimiter
 
 
 class GNPSFileMappingLoader(IFileMappingLoader):
 
     def __init__(self, filename: str):
         self._filename: str = filename
-        self._mapping = load_file_mappings(filename)
-
+        self._mapping: Dict[int, str] = {}
 
     def mapping(self):
         return self._mapping
 
+    def load_mapping_allfiles(self):
+        with open(self._filename, mode='rt', encoding='utf-8') as file:
 
-def load_file_mappings(filename: str) -> Dict[str, str]:
-    with open(filename, mode='rt', encoding='utf-8') as file:
-        csv.reader(file, delimiter='\t')
+            delimiter = find_delimiter(self._filename)
+            reader = csv.reader(file, delimiter=delimiter)
+            header: list[str] = next(reader)
+            reader = csv.DictReader(file, header, delimiter=delimiter)
+ 
+            for row in reader:
+                spectrum_id = int(row["cluster index"])
+                occurrences = row["AllFiles"].split("###") # split by '###'
+                occurrences.pop() # remove last empty entry
 
-        return []
+                # separate the scan position from the files
+                samples = [x.split(':')[0] for x in occurrences]
+
+                self._mapping[spectrum_id] = samples
+
+    def load_mapping_fbmn(self):
+        with open(self._filename, mode='rt', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            line: list[str] = next(reader)
--- tests/test_GNPSFileMappingLoader.py
+++ tests/test_GNPSFileMappingLoader.py
@@ -1,9 +1,26 @@
+import pytest
+
 from nplinker.metabolomics.GNPSFileMappingLoader import GNPSFileMappingLoader
 from tests import DATA_DIR
 
-
-def test_default():
+@pytest.fixture
+def loader() -> GNPSFileMappingLoader:
     filename = DATA_DIR / "nodes.tsv"
-    sut = GNPSFileMappingLoader(filename)
+    return GNPSFileMappingLoader(filename)
 
-    assert sut is not None
+def test_default(loader):
+    assert loader is not None
+
+
+@pytest.mark.parametrize("filename, expected_length, spectrum_id", [
+    [ DATA_DIR / "nodes_mwe.csv", 13, 275],
+    [ DATA_DIR / "nodes.tsv", 25935, 223]
+])
+def test_load_mapping_allfiles(filename, expected_length, spectrum_id):
+    sut = GNPSFileMappingLoader(filename)
+    sut.load_mapping_allfiles()
+
+    actual = sut.mapping()
+
+    assert len(actual) == expected_length
+    assert actual[spectrum_id] == ["26c.mzXML", "26c.mzXML", "26c.mzXML"]
