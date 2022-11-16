66493fa12fd6960c9de2dc56bbd3cca5ff7f8bf6
Cunliang Geng <c.geng@esciencecenter.nl>
2022-11-16 10:43:43 +0100

    add genomics abstract base class

--- src/nplinker/genomics/__init__.py
+++ src/nplinker/genomics/__init__.py
@@ -7,10 +7,11 @@ LogConfig.getLogger(__name__)
 from .bgc import BGC
 from .gcf import GCF
 from .genomics import loadBGC_from_cluster_files
-
+from .abc import BGCLoaderBase
 
 __all__ = [
     "bgc",
     "gcf",
     "loadBGC_from_cluster_files",
+    "BGCLoaderBase"
 ]
