64a5171c0bbe5840c0dcd1d6277797d062103132
hechth <helge.hecht@recetox.muni.cz>
2023-02-22 11:53:40 +0100

    Updated loader to handle both new and old gnps extracted layouts

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import shutil
 import pytest
 from nplinker.loader import DatasetLoader
+from nplinker.metabolomics.gnps.gnps_extractor import GNPSExtractor
 from nplinker.strain_collection import StrainCollection
 from . import DATA_DIR
 
@@ -16,6 +18,22 @@ def config():
         }
     }
 
+@pytest.fixture
+def config_with_new_gnps_extractor():
+    GNPSExtractor(DATA_DIR / "ProteoSAFe-METABOLOMICS-SNETS-c22f44b1-download_clustered_spectra.zip", DATA_DIR / "extracted").extract()
+    yield {
+        "dataset" : {
+            "root": DATA_DIR / "extracted",
+            "platform_id": "",
+            "overrides": {
+                "strain_mappings_file": str(DATA_DIR / "strain_mappings.csv")
+            }
+        }
+    }
+    shutil.rmtree(
+        DATA_DIR /
+        "extracted")
+
 def test_default(config):
     sut = DatasetLoader(config)
     assert sut._platform_id == config["dataset"]["platform_id"]
@@ -28,6 +46,16 @@ def test_has_metabolomics_paths(config):
     assert sut.edges_file == str(config["dataset"]["root"] / "networkedges_selfloop" / "6da5be36f5b14e878860167fa07004d6.pairsinfo")
     assert sut.nodes_file == str(config["dataset"]["root"] / "clusterinfosummarygroup_attributes_withIDs_withcomponentID" / "d69356c8e5044c2a9fef3dd2a2f991e1.tsv")
     assert sut.annotations_dir == str(config["dataset"]["root"] / "result_specnets_DB")
+
+
+def test_has_metabolomics_paths_new_gnps(config_with_new_gnps_extractor):
+    sut = DatasetLoader(config_with_new_gnps_extractor)
+    sut._init_metabolomics_paths()
+    assert sut.mgf_file == str(config_with_new_gnps_extractor["dataset"]["root"] / "spectra.mgf")
+    assert sut.edges_file == str(config_with_new_gnps_extractor["dataset"]["root"] / "molecular_families.pairsinfo")
+    assert sut.nodes_file == str(config_with_new_gnps_extractor["dataset"]["root"] / "file_mappings.tsv")
+    assert sut.annotations_dir == str(config_with_new_gnps_extractor["dataset"]["root"])
+    assert sut.annotations_config_file == str(config_with_new_gnps_extractor["dataset"]["root"] / "annotations.tsv")
 
 
 def test_has_strain_mappings(config):
