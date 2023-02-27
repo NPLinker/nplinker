b0c978aa362d925f1846d1bb9c6c5158aea95f7b
Cunliang Geng <c.geng@esciencecenter.nl>
2023-02-27 15:34:38 +0100

    split function load_gcfs to separate functions

--- src/nplinker/genomics/__init__.py
+++ src/nplinker/genomics/__init__.py
@@ -1,10 +1,25 @@
 import logging
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
+from .abc import BGCLoaderBase
 from .bgc import BGC
 from .gcf import GCF
+from .genomics import filter_mibig_only_gcf
+from .genomics import get_bgcs_from_gcfs
+from .genomics import get_strains_from_bgcs
 from .genomics import load_gcfs
-from .abc import BGCLoaderBase
+from .genomics import map_bgc_to_gcf
+from .genomics import map_strain_to_bgc
 
-__all__ = ["bgc", "gcf", "load_gcfs", "BGCLoaderBase"]
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = [
+    "BGCLoaderBase",
+    "BGC",
+    "GCF",
+    "filter_mibig_only_gcf",
+    "get_bgcs_from_gcfs",
+    "get_strains_from_bgcs",
+    "load_gcfs",
+    "map_bgc_to_gcf",
+    "map_strain_to_bgc"
+]
