4879181203eb4eeb52aec434fac491ee257c3e29
gcroci2 <crocioni.giulia@gmail.com>
2023-03-07 18:23:11 +0100

    properly define download_and_extract_antismash_metadata function

--- src/nplinker/genomics/antismash/__init__.py
+++ src/nplinker/genomics/antismash/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from .antismash_loader import AntismashBGCLoader
 from .antismash_loader import parse_bgc_genbank
-from .antismash_downloader import download_antismash_data
+from .antismash_downloader import download_and_extract_antismash_metadata
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
