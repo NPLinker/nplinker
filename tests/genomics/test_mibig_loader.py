a228640e310a162a2fca5331d9f87d505318ddf5
Cunliang Geng <c.geng@esciencecenter.nl>
2023-03-09 10:58:36 +0100

    add bgc-genome mapping to mibig bgc loader

--- tests/genomics/test_mibig_loader.py
+++ tests/genomics/test_mibig_loader.py
@@ -32,6 +32,13 @@ class TestMibigBGCLoader:
     def test_init(self, loader, data_dir):
         assert loader.data_dir == data_dir
 
+    def test_get_bgc_genome_mapping(self, loader):
+        mapping = loader.get_bgc_genome_mapping()
+        assert isinstance(mapping, dict)
+        assert len(mapping) == 2502
+        for bid in mapping:
+            assert bid == mapping[bid]
+
     def test_get_files(self, loader):
         files = loader.get_files()
         assert isinstance(files, dict)
