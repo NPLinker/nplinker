e9fbe999f0179ac7f035a712090b483cc64e5984
gcroci2 <crocioni.giulia@gmail.com>
2023-03-09 13:33:35 +0100

    make refseq_assembly_id class variable for tests

--- tests/genomics/antismash/test_downloader.py
+++ tests/genomics/antismash/test_downloader.py
@@ -4,13 +4,14 @@ from nplinker.genomics.antismash import download_and_extract_antismash_metadata
 
 class TestDownloadAndExtractAntismashData():
 
+    refseq_assembly_id = "GCF_004339725.1"
+
     def test_default(self, tmp_path):
-        refseq_assembly_id = "GCF_004339725.1"
         download_root = tmp_path / "download"
         download_root.mkdir()
         extract_root = tmp_path / "extracted"
         extract_root.mkdir()
-        download_and_extract_antismash_metadata(refseq_assembly_id,
+        download_and_extract_antismash_metadata(self.refseq_assembly_id,
                                                 download_root, extract_root)
         archive = download_root / "GCF_004339725.1.zip"
         extracted_folder = extract_root / "antismash/GCF_004339725.1"
@@ -19,19 +20,17 @@ class TestDownloadAndExtractAntismashData():
         assert extracted_folder.exists()
 
     def test_error_same_path(self, tmp_path):
-        refseq_assembly_id = "GCF_004339725.1"
         with pytest.raises(ValueError) as e:
-            download_and_extract_antismash_metadata(refseq_assembly_id,
+            download_and_extract_antismash_metadata(self.refseq_assembly_id,
                                                     tmp_path, tmp_path)
         assert e.value.args[
             0] == "Identical path of download directory and extract directory"
 
     def test_error_nonempty_path(self, tmp_path):
-        refseq_assembly_id = "GCF_004339725.1"
-        nonempty_path = tmp_path / f"extracted/antismash/{refseq_assembly_id}/subdir"
+        nonempty_path = tmp_path / f"extracted/antismash/{self.refseq_assembly_id}/subdir"
         nonempty_path.mkdir(parents=True)
         with pytest.raises(ValueError) as e:
-            download_and_extract_antismash_metadata(refseq_assembly_id,
+            download_and_extract_antismash_metadata(self.refseq_assembly_id,
                                                     tmp_path,
                                                     tmp_path / "extracted")
         assert "Nonempty directory" in e.value.args[0]
