b944431be0d38e7102db6bcf5676d320df62be85
gcroci2 <crocioni.giulia@gmail.com>
2023-03-15 17:36:52 +0100

    update init files

--- src/nplinker/genomics/antismash/__init__.py
+++ src/nplinker/genomics/antismash/__init__.py
@@ -1,7 +1,10 @@
 import logging
-from .antismash_loader import AntismashBGCLoader
-from .antismash_loader import parse_bgc_genbank
+from .downloader import download_and_extract_antismash_metadata
+from .loader import AntismashBGCLoader
+from .loader import parse_bgc_genbank
+
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = ["AntismashBGCLoader", "parse_bgc_genbank"]
+__all__ = ["AntismashBGCLoader", "parse_bgc_genbank", "download_and_extract_antismash_metadata"]
