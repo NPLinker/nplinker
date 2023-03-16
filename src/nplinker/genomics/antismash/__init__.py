740f09712386519020a40a2cf107b27b3b37d8a1
gcroci2 <crocioni.giulia@gmail.com>
2023-03-16 09:16:03 +0100

    delete redundant line

--- src/nplinker/genomics/antismash/__init__.py
+++ src/nplinker/genomics/antismash/__init__.py
@@ -6,5 +6,4 @@ from .loader import parse_bgc_genbank
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__all__ = ["AntismashBGCLoader", "parse_bgc_genbank"]
 __all__ = ["AntismashBGCLoader", "parse_bgc_genbank", "download_and_extract_antismash_metadata"]
