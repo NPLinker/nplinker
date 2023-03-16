5e1d7454b2a3c3f6d8f765fead0c2cb31a26782e
gcroci2 <crocioni.giulia@gmail.com>
2023-03-16 19:44:38 +0100

    add suggestions to the antismash downloader test

--- tests/genomics/antismash/test_antismash_downloader.py
+++ tests/genomics/antismash/test_antismash_downloader.py
@@ -1,36 +1,46 @@
 import pytest
-from nplinker.genomics.antismash import download_and_extract_antismash_metadata
+from nplinker.genomics.antismash import download_and_extract_antismash_data
+from nplinker.utils import list_files, extract_archive
 
 
 class TestDownloadAndExtractAntismashData():
 
-    refseq_assembly_id = "GCF_004339725.1"
+    antismash_id = "GCF_004339725.1"
 
     def test_default(self, tmp_path):
         download_root = tmp_path / "download"
         download_root.mkdir()
         extract_root = tmp_path / "extracted"
         extract_root.mkdir()
-        download_and_extract_antismash_metadata(self.refseq_assembly_id,
+        original_extract_root = tmp_path / "original"
+        original_extract_root.mkdir()
+        download_and_extract_antismash_data(self.antismash_id,
                                                 download_root, extract_root)
         archive = download_root / "GCF_004339725.1.zip"
-        extracted_folder = extract_root / "antismash/GCF_004339725.1"
+        extracted_folder = extract_root / "antismash" / "GCF_004339725.1"
+        expected_files = list_files(extracted_folder, suffix=(".json", ".gbk"))
+        all_files = list_files(extracted_folder)
+        # extract zip folder without removing any files
+        extract_archive(archive, original_extract_root)
+        original_expected_files = list_files(original_extract_root, suffix=(".json", ".gbk"))
         assert archive.exists()
         assert archive.is_file()
         assert extracted_folder.exists()
+        assert len(expected_files) == len(all_files)
+        assert len(expected_files) == len(original_expected_files)
 
     def test_error_same_path(self, tmp_path):
         with pytest.raises(ValueError) as e:
-            download_and_extract_antismash_metadata(self.refseq_assembly_id,
+            download_and_extract_antismash_data(self.antismash_id,
                                                     tmp_path, tmp_path)
         assert e.value.args[
             0] == "Identical path of download directory and extract directory"
 
     def test_error_nonempty_path(self, tmp_path):
-        nonempty_path = tmp_path / f"extracted/antismash/{self.refseq_assembly_id}/subdir"
+        nonempty_path = tmp_path / "extracted" / "antismash" / f"{self.antismash_id}" / "subdir"
         nonempty_path.mkdir(parents=True)
         with pytest.raises(ValueError) as e:
-            download_and_extract_antismash_metadata(self.refseq_assembly_id,
+            download_and_extract_antismash_data(self.antismash_id,
                                                     tmp_path,
                                                     tmp_path / "extracted")
         assert "Nonempty directory" in e.value.args[0]
