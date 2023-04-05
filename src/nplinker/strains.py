610d447b397f58c5155fc2a0045527f3a73ea517
Cunliang Geng <c.geng@esciencecenter.nl>
2023-04-05 12:16:36 +0200

    Refactor Strain class (#131)
    * Update .prospector.yml for pylint
    
    * update type hints from Path to PathLike for utils
    
    * remove unused method `Strain.has_alias`
    
    * change `Strain.aliases` from attribute to property
    
    Using property will force user to use method `add_alias` but not directly add alias to the set of aliases.
    
    * add docstring to Strain class
    
    * adjust the orders of methods in Strain class
    
    * update strain unit tests

--- src/nplinker/strains.py
+++ src/nplinker/strains.py
@@ -1,50 +1,54 @@
+from __future__ import annotations
 from .logconfig import LogConfig
+
 
 logger = LogConfig.getLogger(__name__)
 
 
 class Strain():
 
-    def __init__(self, primary_strain_id: str):
-        self.id: str = primary_strain_id
-        self.aliases: set[str] = set()
+    def __init__(self, primary_id: str) -> None:
+        """To model the mapping between strain id and its aliases.
 
-    def has_alias(self, alt_id: str) -> bool:
-        """Check if strain has an alias.
-
-        Args:
-            alt_id(str): Alias to check.
-
-        Returns:
-            bool: Whether the alias is registered in the set of aliases or not.
-        """
-        return alt_id in self.aliases
-
-    def add_alias(self, alt_id: str):
-        """Add an alias to the list of known aliases.
+        It's recommended to use NCBI taxonomy strain id or name as the primary
+        id.
 
         Args:
-            alt_id(str): Alternative id to add to the list of known aliases.
+            primary_id(str): the representative id of the strain.
         """
-        if len(alt_id) == 0:
-            logger.warning(
-                f'Refusing to add zero-length alias to strain {self}')
-            return
-
-        self.aliases.add(alt_id)
+        self.id: str = primary_id
+        self._aliases: set[str] = set()
 
     def __repr__(self) -> str:
         return str(self)
 
     def __str__(self) -> str:
-        return f'Strain({self.id}) [{len(self.aliases)} aliases]'
-    
-    def __eq__(self, other):
-        return (
-            isinstance(other, Strain)
-            and self.id == other.id
-            and self.aliases == other.aliases
-        )
-    
+        return f'Strain({self.id}) [{len(self._aliases)} aliases]'
+
+    def __eq__(self, other) -> bool:
+        return (isinstance(other, Strain) and self.id == other.id
+                and self._aliases == other._aliases)
+
     def __hash__(self) -> int:
         return hash(self.id)
+
+    @property
+    def aliases(self) -> set[str]:
+        """Get the set of known aliases.
+
+        Returns:
+            set[str]: A set of aliases associated with the strain.
+        """
+        return self._aliases
+
+    def add_alias(self, alias: str) -> None:
+        """Add an alias to the list of known aliases.
+
+        Args:
+            alias(str): The alias to add to the list of known aliases.
+        """
+        if len(alias) == 0:
+            logger.warning(
+                'Refusing to add an empty-string alias to strain {%s}', self)
+        else:
+            self._aliases.add(alias)
--- tests/test_strain.py
+++ tests/test_strain.py
@@ -1,28 +1,35 @@
-import pytest
 from nplinker.strains import Strain
 
 
 def test_default():
-    sut = Strain("peter")
-    assert sut.id == "peter"
+    sut = Strain("strain_1")
+    assert sut.id == "strain_1"
+    assert isinstance(sut.aliases, set)
     assert len(sut.aliases) == 0
 
 
-@pytest.mark.parametrize("alias, expected", [
-    ["dieter", True],
-    ["ulrich", False]
-])
-def test_has_alias(strain: Strain, alias: str, expected: bool):
-    assert strain.has_alias(alias) == expected
+def test_repr(strain: Strain):
+    assert repr(strain) == "Strain(strain_1) [1 aliases]"
+
+
+def test_str(strain: Strain):
+    assert str(strain) == "Strain(strain_1) [1 aliases]"
+
+
+def test_eq(strain: Strain):
+    other = Strain("strain_1")
+    other.add_alias("strain_1_a")
+    assert strain == other
+
+
+def test_hash(strain: Strain):
+    assert hash(strain) == hash("strain_1")
+
+
+def test_alias(strain: Strain):
+    assert len(strain.aliases) == 1
 
 
 def test_add_alias(strain: Strain):
-    strain.add_alias("test")
+    strain.add_alias("strain_1_b")
     assert len(strain.aliases) == 2
-
-
-def test_equal(strain: Strain):
-    other = Strain("peter")
-    other.add_alias("dieter")
-
-    assert strain == other
