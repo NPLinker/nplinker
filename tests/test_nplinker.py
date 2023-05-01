ee5ff03f07c31a477555a75d6aef863c8796c3c0
Cunliang Geng <c.geng@esciencecenter.nl>
2023-05-01 16:38:35 +0200

    Update test_nplinker.py
    - add code to remove cached results

--- tests/test_nplinker.py
+++ tests/test_nplinker.py
@@ -1,33 +1,39 @@
+import os
+from pathlib import Path
 import pytest
 from nplinker.nplinker import NPLinker
-import os
-
 from . import DATA_DIR
 
+
 @pytest.fixture(scope='module')
-def instance() -> NPLinker:
+def npl() -> NPLinker:
     npl = NPLinker(str(DATA_DIR / 'nplinker_demo1.toml'))
     npl.load_data()
+    # remove cached results before running tests
+    root_dir = Path(npl.root_dir)
+    score_cache = root_dir / 'metcalf' / 'metcalf_scores.pckl'
+    score_cache.unlink(missing_ok=True)
     return npl
 
 
-@pytest.mark.skipif(os.environ.get('CI') == 'true', reason="Skip when running on CI")
-def test_load_data(instance: NPLinker):
-    assert len(instance.bgcs) == 390
-    assert len(instance.gcfs) == 113
-    assert len(instance.spectra) == 25935
-    assert len(instance.molfams) == 25769
+@pytest.mark.skipif(os.environ.get('CI') == 'true',
+                    reason="Skip when running on CI")
+def test_load_data(npl: NPLinker):
+    assert len(npl.bgcs) == 390
+    assert len(npl.gcfs) == 113
+    assert len(npl.spectra) == 25935
+    assert len(npl.molfams) == 25769
 
 
-@pytest.mark.skipif(os.environ.get('CI') == 'true' , reason="Skip when running on CI")
-def test_get_links(instance: NPLinker):
-    mc = instance.scoring_method('metcalf')
+@pytest.mark.skipif(os.environ.get('CI') == 'true',
+                    reason="Skip when running on CI")
+def test_get_links(npl: NPLinker):
+    mc = npl.scoring_method('metcalf')
     mc.cutoff = 3.5
     mc.standardised = True
 
-    actual = instance.get_links(instance.gcfs, mc, and_mode=True)
+    actual = npl.get_links(npl.gcfs, mc, and_mode=True)
     assert len(actual) == len(actual.sources) == len(actual.links) == 101
-    
+
     actual.filter_links(lambda link: link[mc] > 5.0)
     assert len(actual.links) == 60
-
