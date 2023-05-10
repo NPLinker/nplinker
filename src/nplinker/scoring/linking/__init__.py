ac94cd867067a5f9caadee04bde3a5c3724333ca
Cunliang Geng <c.geng@esciencecenter.nl>
2023-05-10 09:48:24 +0200

    rename `data_linking.py` to `data_links.py`

--- src/nplinker/scoring/linking/__init__.py
+++ src/nplinker/scoring/linking/__init__.py
@@ -1,6 +1,6 @@
 import logging
-from .data_linking import DataLinks
-from .data_linking import LINK_TYPES
+from .data_links import DataLinks
+from .data_links import LINK_TYPES
 from .data_linking_functions import calc_correlation_matrix
 from .link_finder import LinkFinder
 
