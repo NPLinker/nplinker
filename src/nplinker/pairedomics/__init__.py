05b5964f7acb4e452d467f0fc6c2e714d3e4cc54
Giulia Crocioni <55382553+gcroci2@users.noreply.github.com>
2023-05-12 12:08:03 +0200

    Update src/nplinker/pairedomics/__init__.py
    Co-authored-by: Cunliang Geng <c.geng@esciencecenter.nl>

--- src/nplinker/pairedomics/__init__.py
+++ src/nplinker/pairedomics/__init__.py
@@ -1,2 +1,10 @@
+import logging
 from .podp_antismash_downloader import download_antismash_data
 from .podp_antismash_downloader import podp_download_and_extract_antismash_data
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = [
+	"download_antismash_data",
+	"podp_download_and_extract_antismash_data"
+]
