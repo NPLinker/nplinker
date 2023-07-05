a91d16d3ec5674df7d49b2e8e8eab23f650b8e74
Cunliang Geng <c.geng@esciencecenter.nl>
2023-07-05 16:02:27 +0200

    add unit tests for new methods

--- tests/test_strain_collection.py
+++ tests/test_strain_collection.py
@@ -1,7 +1,7 @@
+import json
 import pytest
 from nplinker.strain_collection import StrainCollection
 from nplinker.strains import Strain
-from tests import DATA_DIR
 
 
 @pytest.fixture
@@ -73,3 +73,57 @@ def test_lookup(collection: StrainCollection, strain: Strain):
         assert collection.lookup(alias) == strain
     with pytest.raises(KeyError):
         collection.lookup("strain_not_exist")
+
+
+@pytest.fixture
+def json_file(tmp_path):
+    data = {
+        "strain_mappings": [{
+            "strain_id": "strain_1",
+            "strain_alias": ["alias_1", "alias_2"]
+        }, {
+            "strain_id": "strain_2",
+            "strain_alias": ["alias_3", "alias_4"]
+        }]
+    }
+    file_path = tmp_path / "test.json"
+    with open(file_path, "w") as f:
+        json.dump(data, f)
+    return file_path
+
+
+def test_read_json(json_file):
+    expected_strain_1 = Strain("strain_1")
+    expected_strain_1.add_alias("alias_1")
+    expected_strain_1.add_alias("alias_2")
+    expected_strain_2 = Strain("strain_2")
+    expected_strain_2.add_alias("alias_3")
+    expected_strain_2.add_alias("alias_4")
+    expected_collection = StrainCollection()
+    expected_collection.add(expected_strain_1)
+    expected_collection.add(expected_strain_2)
+
+    actual_collection = StrainCollection.read_json(json_file)
+    assert actual_collection == expected_collection
+
+
+def test_to_json(collection: StrainCollection, tmp_path):
+    # tests writing to string
+    expected_data = {
+        "strain_mappings": [{
+            "strain_id": "strain_1",
+            "strain_alias": ["strain_1_a"]
+        }],
+        "version":
+        "1.0"
+    }
+    expected_json = json.dumps(expected_data)
+    actual_json = collection.to_json()
+    assert actual_json == expected_json
+
+    # tests writing to file
+    file_path = tmp_path / "test.json"
+    collection.to_json(file_path)
+    with open(file_path, "r") as f:
+        actual_data = json.load(f)
+    assert actual_data == expected_data
