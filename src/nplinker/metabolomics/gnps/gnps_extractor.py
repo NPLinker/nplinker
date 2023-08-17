5a26b28d3cda1cbcd442ecb20fffebb1856027bc
Cunliang Geng <c.geng@esciencecenter.nl>
2023-08-17 09:49:44 +0200

    format gnps_extractor.py file

--- src/nplinker/metabolomics/gnps/gnps_extractor.py
+++ src/nplinker/metabolomics/gnps/gnps_extractor.py
@@ -1,17 +1,18 @@
-from os import PathLike
 import os
+from os import PathLike
 from pathlib import Path
 import shutil
 import zipfile
-
 from nplinker import utils
-from nplinker.metabolomics.gnps.gnps_format import GNPSFormat, gnps_format_from_archive
+from nplinker.metabolomics.gnps.gnps_format import gnps_format_from_archive
+from nplinker.metabolomics.gnps.gnps_format import GNPSFormat
 
 
 class GNPSExtractor:
+
     def __init__(self, file: str | PathLike, extract_path: str | PathLike):
         """Class to extract files from a GNPS output archive.
-        
+
         The GNPS output archive contains files for spectra, molecular families and file mappings.
 
         Args:
@@ -21,7 +22,6 @@ class GNPSExtractor:
         self._file: Path = Path(file)
         self._extract_path: Path = Path(extract_path)
         self._is_fbmn = gnps_format_from_archive(self._file) == GNPSFormat.FBMN
-
 
     def get_data(self) -> zipfile.ZipFile:
         """Get the file object of the archive.
@@ -48,8 +48,9 @@ class GNPSExtractor:
 
     def _extract_spectra(self):
         """Helper function to extract the spectra file from the archive."""
-        prefix = "spectra" if self._is_fbmn else ""            
-        utils.extract_file_matching_pattern(self.get_data(), prefix, ".mgf", self._extract_path, "spectra.mgf")
+        prefix = "spectra" if self._is_fbmn else ""
+        utils.extract_file_matching_pattern(self.get_data(), prefix, ".mgf",
+                                            self._extract_path, "spectra.mgf")
         if self._is_fbmn:
             os.rmdir(self._extract_path / prefix)
 
@@ -57,37 +58,25 @@ class GNPSExtractor:
         """Helper function to extract the molecular families file from the archive."""
         prefix = "networkedges_selfloop"
         suffix = ".selfloop" if self._is_fbmn else ".pairsinfo"
-        utils.extract_file_matching_pattern(
-            self.get_data(),
-            prefix,
-            suffix,
-            self._extract_path,
-            "molecular_families.pairsinfo"
-        )
+        utils.extract_file_matching_pattern(self.get_data(), prefix, suffix,
+                                            self._extract_path,
+                                            "molecular_families.pairsinfo")
         os.rmdir(self._extract_path / prefix)
-    
+
     def _extract_file_mappings(self):
         """Helper function to extract the file mappings file from the archive."""
         prefix = "quantification_table_reformatted" if self._is_fbmn else "clusterinfosummarygroup_attributes_withIDs_withcomponentID"
         suffix = ".csv" if self._is_fbmn else ".tsv"
-        utils.extract_file_matching_pattern(
-            self.get_data(),
-            prefix,
-            suffix,
-            self._extract_path,
-            "file_mappings" + suffix
-        )
+        utils.extract_file_matching_pattern(self.get_data(), prefix, suffix,
+                                            self._extract_path,
+                                            "file_mappings" + suffix)
         os.rmdir(self._extract_path / prefix)
 
     def _extract_annotations(self):
         """Helper function to extract the annotations file from the archive."""
-        prefix = "DB_result" if self._is_fbmn else "result_specnets_DB"            
-        utils.extract_file_matching_pattern(
-            self.get_data(),
-            prefix,
-            ".tsv",
-            self._extract_path,
-            "annotations.tsv"
-        )
-        
-        shutil.rmtree(self._extract_path / prefix)        
+        prefix = "DB_result" if self._is_fbmn else "result_specnets_DB"
+        utils.extract_file_matching_pattern(self.get_data(), prefix, ".tsv",
+                                            self._extract_path,
+                                            "annotations.tsv")
+
+        shutil.rmtree(self._extract_path / prefix)
