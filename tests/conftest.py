894cfef7d045939b628f556dcc3cd74abd2ce725
Cunliang Geng <c.geng@esciencecenter.nl>
2023-08-17 14:29:21 +0200

    create conftest.py for metabolomics tests
    Move some fixtures from top level to metabolomics level

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,43 +3,17 @@ import shutil
 import pytest
 from nplinker.globals import STRAIN_MAPPINGS_FILENAME
 from nplinker.metabolomics.metabolomics import load_spectra
-from nplinker.metabolomics.metabolomics import make_families
 from nplinker.metabolomics.spectrum import Spectrum
 from nplinker.strain_collection import StrainCollection
 from nplinker.strains import Strain
-from nplinker.utils import extract_archive
 from . import DATA_DIR
-
-
-def _unpack(archive: Path):
-    filepath = DATA_DIR / archive
-    outdir = DATA_DIR / filepath.stem
-    extract_archive(filepath, outdir)
-    return filepath, outdir
-
-
-@pytest.fixture(scope="session", autouse=True)
-def prepare_data():
-    _unpack(
-        "ProteoSAFe-METABOLOMICS-SNETS-c22f44b1-download_clustered_spectra.zip"
-    )
-    _unpack(
-        "ProteoSAFe-FEATURE-BASED-MOLECULAR-NETWORKING-92036537-download_cytoscape_data.zip"
-    )
-    yield
-    shutil.rmtree(
-        DATA_DIR /
-        "ProteoSAFe-METABOLOMICS-SNETS-c22f44b1-download_clustered_spectra")
-    shutil.rmtree(
-        DATA_DIR /
-        "ProteoSAFe-FEATURE-BASED-MOLECULAR-NETWORKING-92036537-download_cytoscape_data"
-    )
+from . import GNPS_DATA_DIR
 
 
 @pytest.fixture
 def spec_dict() -> dict[str, Spectrum]:
-    mgf_file = DATA_DIR / "spectra.mgf"
-    edges_file = DATA_DIR / "edges.pairsinfo"
+    mgf_file = GNPS_DATA_DIR / "spectra.mgf"
+    edges_file = GNPS_DATA_DIR / "edges.pairsinfo"
     return load_spectra(mgf_file, edges_file)
 
 
