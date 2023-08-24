778cf4bfd0342430a0bea1d9923a3760530c44f6
Cunliang Geng <c.geng@esciencecenter.nl>
2023-08-24 14:47:36 +0200

    Update test_gnps_spectrum_loader.py

--- tests/metabolomics/test_gnps_spectrum_loader.py
+++ tests/metabolomics/test_gnps_spectrum_loader.py
@@ -1,12 +1,11 @@
 import pytest
+from nplinker.metabolomics.gnps.gnps_format import GNPSFormat
 from nplinker.metabolomics.gnps.gnps_spectrum_loader import GNPSSpectrumLoader
 
-from .. import DATA_DIR
 
-@pytest.mark.parametrize("file, expected", [
-    [DATA_DIR / "ProteoSAFe-METABOLOMICS-SNETS-c22f44b1-download_clustered_spectra/METABOLOMICS-SNETS-c22f44b1-download_clustered_spectra-main.mgf", 435],
-    [DATA_DIR / "ProteoSAFe-FEATURE-BASED-MOLECULAR-NETWORKING-92036537-download_cytoscape_data/spectra/specs_ms.mgf", 1492]
-])
-def test_loads_spectra(file, expected):
-    actual = GNPSSpectrumLoader(file).spectra()
-    assert len(actual) == expected
+@pytest.mark.parametrize("workflow, num_spectra",
+                         [[GNPSFormat.FBMN, 1492], [GNPSFormat.SNETS, 25935],
+                          [GNPSFormat.SNETSV2, 7383]])
+def test_gnps_spectrum_loader(workflow, num_spectra, gnps_spectra_files):
+    loader = GNPSSpectrumLoader(gnps_spectra_files[workflow])
+    assert len(loader.spectra) == num_spectra
