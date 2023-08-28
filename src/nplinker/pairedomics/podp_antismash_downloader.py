b4a47830a9ff5bf820498b131211953e7f0ad143
Cunliang Geng <c.geng@esciencecenter.nl>
2023-08-28 12:10:24 +0200

    replace HTTPError with more robust Exception

--- src/nplinker/pairedomics/podp_antismash_downloader.py
+++ src/nplinker/pairedomics/podp_antismash_downloader.py
@@ -3,7 +3,6 @@ from os import PathLike
 from pathlib import Path
 import re
 import time
-from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
 from bs4 import Tag
@@ -203,7 +202,7 @@ def podp_download_and_extract_antismash_data(
             if output_path.exists():
                 Path.touch(output_path / 'completed', exist_ok=True)
 
-        except HTTPError:
+        except Exception:
             gs_obj.bgc_path = ""
 
     missing = len([gs for gs in gs_dict.values() if not gs.bgc_path])
