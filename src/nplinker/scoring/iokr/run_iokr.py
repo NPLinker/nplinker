952fd7871f3ea908a754a969ca6473113298ca91
Cunliang Geng <c.geng@esciencecenter.nl>
2023-11-02 15:50:40 +0100

    format code again using ruff
    run command `ruff format .`

--- src/nplinker/scoring/iokr/run_iokr.py
+++ src/nplinker/scoring/iokr/run_iokr.py
@@ -138,7 +138,11 @@ def run_iokr(data):
     print("")
     print("IOKR test run done!")
     print(f"#samples: {len(collected_rankings)}")
-    print("top-1 acc: {}".format(float([x[1] for x in collected_rankings].count(0)) / total, ))
+    print(
+        "top-1 acc: {}".format(
+            float([x[1] for x in collected_rankings].count(0)) / total,
+        )
+    )
 
     return collected_rankings
 
