75ff1bbbaf57a5c421b673d59d0aa24235c29dd6
Cunliang Geng <c.geng@esciencecenter.nl>
2023-11-22 09:18:40 +0100

    change property `families` to method `get_mfs`
    - change property to method
    - rename `families` to `get_mfs`

--- tests/metabolomics/test_gnps_molecular_family_loader.py
+++ tests/metabolomics/test_gnps_molecular_family_loader.py
@@ -9,7 +9,7 @@ from nplinker.metabolomics.gnps import GNPSMolecularFamilyLoader
 )
 def test_has_molecular_families(workflow, num_families, num_spectra, gnps_mf_files):
     loader = GNPSMolecularFamilyLoader(gnps_mf_files[workflow])
-    actual = loader.families
+    actual = loader.get_mfs
     assert len(actual) == num_families
     # test molecular family with id "1" has correct number of spectra ids
     mf = [mf for mf in actual if mf.family_id == "1"][0]
