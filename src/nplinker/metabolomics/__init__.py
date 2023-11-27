020a028714c40d6280418b570c61269cb77a9f1d
Cunliang Geng <c.geng@esciencecenter.nl>
2023-11-27 15:28:14 +0100

    remove class `SingletonFamily`
    Class MolecularFamily can be used to represent singleton molecular family.

--- src/nplinker/metabolomics/__init__.py
+++ src/nplinker/metabolomics/__init__.py
@@ -1,10 +1,9 @@
 import logging
 from .molecular_family import MolecularFamily
-from .singleton_family import SingletonFamily
 from .spectrum import GNPS_KEY
 from .spectrum import Spectrum
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__all__ = ["MolecularFamily", "SingletonFamily", "GNPS_KEY", "Spectrum"]
+__all__ = ["MolecularFamily", "GNPS_KEY", "Spectrum"]
