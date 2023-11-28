2716ecb1fa5aebbc2b6e698aab9b892a829477c4
Cunliang Geng <c.geng@esciencecenter.nl>
2023-11-28 11:51:25 +0100

    remove parameter and attribute `parent_mz`
    `parent_mz` refers to `precursor_mz` in the context, so removing `parent_mz` and keep `precursor_mz`.

--- tests/metabolomics/test_spectrum.py
+++ tests/metabolomics/test_spectrum.py
@@ -4,9 +4,7 @@ from nplinker.metabolomics import Spectrum
 
 @pytest.fixture
 def spectrum() -> Spectrum:
-    spec = Spectrum(
-        1, peaks=[[10, 100], [20, 150]], spectrum_id="2", precursor_mz=30, parent_mz=50, rt=100
-    )
+    spec = Spectrum(1, peaks=[[10, 100], [20, 150]], spectrum_id="2", precursor_mz=30, rt=100)
     return spec
 
 
