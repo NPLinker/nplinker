d3b9eb9e8fca88ed1fb9b775d3a9613822fbfa35
Cunliang Geng <c.geng@esciencecenter.nl>
2023-12-05 16:44:55 +0100

    rename function `map_bgc_to_gcf` to `add_bgc_to_gcf`
    - rename function
    - update typing

--- src/nplinker/genomics/__init__.py
+++ src/nplinker/genomics/__init__.py
@@ -2,11 +2,11 @@ import logging
 from .abc import BGCLoaderBase
 from .bgc import BGC
 from .gcf import GCF
+from .utils import add_bgc_to_gcf
 from .utils import add_strain_to_bgc
 from .utils import generate_mappings_genome_id_bgc_id
 from .utils import get_bgcs_from_gcfs
 from .utils import get_strains_from_bgcs
-from .utils import map_bgc_to_gcf
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
@@ -18,6 +18,6 @@ __all__ = [
     "generate_mappings_genome_id_bgc_id",
     "get_bgcs_from_gcfs",
     "get_strains_from_bgcs",
-    "map_bgc_to_gcf",
+    "add_bgc_to_gcf",
     "add_strain_to_bgc",
 ]
