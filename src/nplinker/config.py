4053946f4419acf2772b5a665c56c1d330540250
Cunliang Geng <c.geng@esciencecenter.nl>
2024-02-23 10:14:08 +0100

    add validations for all settings

--- src/nplinker/config.py
+++ src/nplinker/config.py
@@ -27,10 +27,46 @@ config = Dynaconf(settings_files=[user_config_file], preload=[default_config_fil
 # Validataor parameter `required=False` means the setting (e.g. "loglevel") must not exist rather
 # than being optional. So don't set the parameter `required` if the key is optional.
 validators = [
-    Validator("loglevel", is_type_of=str),
-    Validator("logfile", is_type_of=str),
-    Validator("repro_file", is_type_of=str),
-    Validator("log_to_stdout", is_type_of=bool),
+    # General settings
+    ## `root_dir` value is transformed to a `pathlib.Path` object and must be a directory.
+    Validator("root_dir", required=True, cast=Path, condition=lambda v: v.is_dir()),
+    Validator("mode", required=True, cast=lambda v: v.lower(), is_in=["local", "podp"]),
+    ## `podp_id` must be set if `mode` is "podp"; must not be set if `mode` is "local".
+    Validator("podp_id", required=True, when=Validator("mode", eq="podp")),
+    Validator("podp_id", required=False, when=Validator("mode", eq="local")),
+    # Log
+    ## `loglevel` must be a string and must be one of the supported levels. It is transformed to
+    ## uppercase to avoid case sensitivity.
+    Validator(
+        "log.level",
+        is_type_of=str,
+        cast=lambda v: v.upper(),
+        is_in=["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    ),
+    Validator("log.file", is_type_of=str, cast=Path),
+    Validator("log.to_stdout", is_type_of=bool),
+    #  Mibig
+    Validator("mibig.to_use", required=True, is_type_of=bool),
+    Validator(
+        "mibig.version",
+        required=True,
+        is_type_of=str,
+        when=Validator("mibig.to_use", eq=True),
+    ),
+    # BigScape
+    Validator("bigscape.parameters", required=True, is_type_of=str),
+    Validator("bigscape.cutoff", required=True, is_type_of=float),
+    # Scoring
+    ## `scoring.methods` must be a list of strings and must contain at least one of the
+    ## supported scoring methods.
+    Validator(
+        "scoring.methods",
+        required=True,
+        cast=lambda v: [i.lower() for i in v],
+        is_type_of=list,
+        len_min=1,
+        condition=lambda v: set(v).issubset({"metcalf", "rosetta"}),
+    ),
 ]
 config.validators.register(*validators)
 config.validators.validate()
