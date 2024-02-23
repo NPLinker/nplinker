b161366866cb60053288e4733f66e797aa748027
Cunliang Geng <c.geng@esciencecenter.nl>
2024-02-23 15:22:23 +0100

    Update globals.py

--- src/nplinker/globals.py
+++ src/nplinker/globals.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from pathlib import Path
 from nplinker.config import config
 
 
@@ -11,8 +12,8 @@ GENOME_STATUS_FILENAME = "genome_status.json"
 GNPS_FILE_MAPPINGS_FILENAME = "file_mappings.tsv"
 
 
-DOWNLOADS_DEFAULT_PATH: os.PathLike = config.root_dir / "downloads"
-MIBIG_DEFAULT_PATH: os.PathLike = config.root_dir / "mibig"
-GNPS_DEFAULT_PATH: os.PathLike = config.root_dir / "gnps"
-ANTISMASH_DEFAULT_PATH: os.PathLike = config.root_dir / "antismash"
-BIGSCAPE_DEFAULT_PATH: os.PathLike = config.root_dir / "bigscape"
+DOWNLOADS_DEFAULT_PATH: Path = config.root_dir / "downloads"
+MIBIG_DEFAULT_PATH: Path = config.root_dir / "mibig"
+GNPS_DEFAULT_PATH: Path = config.root_dir / "gnps"
+ANTISMASH_DEFAULT_PATH: Path = config.root_dir / "antismash"
+BIGSCAPE_DEFAULT_PATH: Path = config.root_dir / "bigscape"
