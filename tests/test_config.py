34e09795ea7714291c73fd30a21ffdef520ad0bb
Cunliang Geng <c.geng@esciencecenter.nl>
2024-02-23 10:32:41 +0100

    update unit test for new configs

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,23 +1,27 @@
-from dynaconf import Dynaconf
 from nplinker.config import config
-from . import DATA_DIR
 
 
 def test_config_demo1():
     """Test loading the default config file (nplinker_demo1.toml)."""
-    # the file "nplinker_demo1.toml" is set in ./conftest.py
-    assert config.loglevel == "DEBUG"
-    assert config["loglevel"] == "DEBUG"
-    assert config.get("loglevel") == "DEBUG"
+    # The file "nplinker_demo1.toml" is set in ./conftest.py
 
-    assert config.logfile == ""
-    assert config.repro_file == ""
+    assert config.mode == "podp"
+    assert config.podp_id == "4b29ddc3-26d0-40d7-80c5-44fb6631dbf9.4"
 
-    assert config.dataset.root == "platform:MSV000079284"
-    assert config["dataset.root"] == "platform:MSV000079284"
-    assert config.get("dataset.root") == "platform:MSV000079284"
+    # The following are default values from nplinker_default.toml
+    assert config.log.level == "INFO"
+    assert config["log.level"] == "INFO"
+    assert config.get("log.level") == "INFO"
+    assert config.get("log.file") is None
+    assert config.log.to_stdout is True
 
-    assert config.dataset.platform_id == "MSV000079284"
-    assert config.webapp.tables_metcalf_score == 3.0
+    assert config.mibig.to_use is True
+    assert config.mibig.version == "3.1"
 
+    assert (
+        config.bigscape.parameters
+        == "--mibig --clans-off --mix --include_singletons --cutoffs 0.30"
+    )
+    assert config.bigscape.cutoff == 0.30
 
+    assert config.scoring.methods == ["metcalf"]
