a1cb7d69e97b03954825651f1c05d462228a61ce
Cunliang Geng <c.geng@esciencecenter.nl>
2024-02-23 14:47:57 +0100

    add default data paths to globals

--- src/nplinker/globals.py
+++ src/nplinker/globals.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from nplinker.config import config
 
 
 PFAM_PATH = os.path.join(sys.prefix, "nplinker_lib")
@@ -8,3 +9,10 @@ STRAIN_MAPPINGS_FILENAME = "strain_mappings.json"
 GENOME_BGC_MAPPINGS_FILENAME = "genome_bgc_mappings.json"
 GENOME_STATUS_FILENAME = "genome_status.json"
 GNPS_FILE_MAPPINGS_FILENAME = "file_mappings.tsv"
+
+
+DOWNLOADS_DEFAULT_PATH: os.PathLike = config.root_dir / "downloads"
+MIBIG_DEFAULT_PATH: os.PathLike = config.root_dir / "mibig"
+GNPS_DEFAULT_PATH: os.PathLike = config.root_dir / "gnps"
+ANTISMASH_DEFAULT_PATH: os.PathLike = config.root_dir / "antismash"
+BIGSCAPE_DEFAULT_PATH: os.PathLike = config.root_dir / "bigscape"
