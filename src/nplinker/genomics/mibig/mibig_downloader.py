10c37a62e31b0e4cebe879b7a47fa4a360252d9b
Cunliang Geng <c.geng@esciencecenter.nl>
2024-03-01 15:53:41 +0100

    Update mibig downloader
    add creation of mibig dir if not exist
    
    add the support of PathLike

--- src/nplinker/genomics/mibig/mibig_downloader.py
+++ src/nplinker/genomics/mibig/mibig_downloader.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 from nplinker.logconfig import LogConfig
 from nplinker.utils import download_and_extract_archive
 from nplinker.utils import list_dirs
@@ -23,31 +24,37 @@ _MD5_MIBIG_METADATA = {
 
 
 def download_and_extract_mibig_metadata(
-    download_root: str,
-    extract_path: str,
+    download_root: str | os.PathLike,
+    extract_path: str | os.PathLike,
     version: str = "3.1",
 ):
     """Download and extract MIBiG metadata json files.
 
+        Note that it does not matter whether the metadata json files are in nested folders or not in the archive,
+        all json files will be extracted to the same location, i.e. `extract_path`. The nested
+        folders will be removed if they exist. So the `extract_path` will have only json files.
+
     Args:
-        download_root(str): Path to the directory to place downloaded archive in
-        extract_path(str): Path to the empty directory metadata json files will
-            be extracted to.
-            Note that if it's a directory containing the metadata json files
-            after extraction, the json files will be moved to `extract_path`,
-            and then the extracted directory will be removed.
+        download_root(str | os.PathLike): Path to the directory in which to place the downloaded archive.
+        extract_path(str | os.PathLike): Path to an empty directory where the json files will be extracted.
+            The directory must be empty if it exists. If it doesn't exist, the directory will be created.
         version (str, optional): _description_. Defaults to "3.1".
 
     Examples:
         >>> download_and_extract_mibig_metadata("/data/download", "/data/mibig_metadata")
     """
+    download_root = Path(download_root)
+    extract_path = Path(extract_path)
+
     if download_root == extract_path:
         raise ValueError("Identical path of download directory and extract directory")
 
     # check if extract_path is empty
-    files = [i for i in os.listdir(extract_path)]
-    if len(files) != 0:
-        raise ValueError(f'Nonempty directory: "{extract_path}"')
+    if not extract_path.exists():
+        extract_path.mkdir(parents=True)
+    else:
+        if len(list(extract_path.iterdir())) != 0:
+            raise ValueError(f'Nonempty directory: "{extract_path}"')
 
     # download and extract
     md5 = _MD5_MIBIG_METADATA[version]
