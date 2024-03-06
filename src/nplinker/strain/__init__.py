85641511cebd12b6cd0944b54168c70d8c986b05
Cunliang Geng <c.geng@esciencecenter.nl>
2024-03-06 10:07:59 +0100

    move strain related modules to strain folder

--- src/nplinker/strain/__init__.py
+++ src/nplinker/strain/__init__.py
@@ -1,4 +1,7 @@
 import logging
+from .strain import Strain
+from .strain_collection import StrainCollection
+from .strain_loader import load_user_strains
 from .strain_mappings_generator import extract_mappings_ms_filename_spectrum_id
 from .strain_mappings_generator import extract_mappings_original_genome_id_resolved_genome_id
 from .strain_mappings_generator import extract_mappings_resolved_genome_id_bgc_id
@@ -12,12 +15,15 @@ from .strain_mappings_generator import podp_generate_strain_mappings
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = [
-    "podp_generate_strain_mappings",
-    "extract_mappings_strain_id_original_genome_id",
+    "Strain",
+    "StrainCollection",
+    "load_user_strains",
+    "extract_mappings_ms_filename_spectrum_id",
     "extract_mappings_original_genome_id_resolved_genome_id",
     "extract_mappings_resolved_genome_id_bgc_id",
-    "get_mappings_strain_id_bgc_id",
     "extract_mappings_strain_id_ms_filename",
-    "extract_mappings_ms_filename_spectrum_id",
+    "extract_mappings_strain_id_original_genome_id",
+    "get_mappings_strain_id_bgc_id",
     "get_mappings_strain_id_spectrum_id",
+    "podp_generate_strain_mappings",
 ]
