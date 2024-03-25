75932c28a0f0a23610659252ef9d72aaabdfaeae
CunliangGeng <CunliangGeng@users.noreply.github.com>
2024-03-25 16:45:16 +0000

    Update version to 2.0.0-alpha.1

--- src/nplinker/__init__.py
+++ src/nplinker/__init__.py
@@ -5,4 +5,4 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Cunliang Geng"
 __email__ = "c.geng@esciencecenter.nl"
-__version__ = "1.3.2"
+__version__ = "2.0.0-alpha.1"
