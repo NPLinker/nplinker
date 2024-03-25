a653781dd5fb70d52760df508ddd137af182803d
Cunliang Geng <c.geng@esciencecenter.nl>
2024-03-25 17:11:08 +0100

    move dependencies to pyproject.toml file
    Major changes
    - move dependencies to pyproject.toml file
    - remove files `requirements.txt` and `requirements.dev.txt`

--- setup.py
+++ setup.py
@@ -6,5 +6,4 @@ from setuptools import setup
 if sys.version_info[:2] < (3, 9):
     raise RuntimeError("Python version >= 3.9 required.")
 
-# see setup.cfg
 setup(scripts=["bin/install-nplinker-deps"])
