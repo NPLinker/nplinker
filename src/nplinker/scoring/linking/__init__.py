8bb47613568826fc0059eeca7dab8a81d21f1b15
Cunliang Geng <c.geng@esciencecenter.nl>
2024-05-29 16:48:07 +0200

    remove LinkFinder

--- src/nplinker/scoring/linking/__init__.py
+++ src/nplinker/scoring/linking/__init__.py
@@ -1,8 +1,7 @@
 from .data_links import LINK_TYPES
 from .data_links import DataLinks
-from .link_finder import LinkFinder
 from .utils import calc_correlation_matrix
 from .utils import isinstance_all
 
 
-__all__ = ["DataLinks", "LINK_TYPES", "LinkFinder", "calc_correlation_matrix", "isinstance_all"]
+__all__ = ["DataLinks", "LINK_TYPES", "calc_correlation_matrix", "isinstance_all"]
