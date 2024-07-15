a2b6eb87c24af9bf902a465c5b11d7efe7a96e2f
Arjan Draisma <arjan.draisma@wur.nl>
2024-07-15 12:16:17 +0200

    add info to docstring

--- src/nplinker/genomics/bigscape/runbigscape.py
+++ src/nplinker/genomics/bigscape/runbigscape.py
@@ -16,12 +16,28 @@ def run_bigscape(
     output_path: str | PathLike,
     extra_params: str,
     version: int = 1,
-):
+) -> bool:
     """Runs BiG-SCAPE to cluster BGCs.
 
     The behavior of this function is slightly different depending on the version of
     BiG-SCAPE that is set to run using the configuration file.
     Mostly this means a different set of parameters is used between the two versions.
+
+    The AntiSMASH output directory should be a directory that contains GBK files.
+    The directory can contain subdirectories, in which case BiG-SCAPE will search
+    recursively for GBK files.
+
+    By default, only GBK Files with "cluster" or "region" in the filename are
+    accepted. GBK Files with "final" in the filename are excluded.
+
+    Args:
+        antismash_path: Path to the antismash output directory.
+        output_path: Path to the output directory where BiG-SCAPE will write its results.
+        extra_params: Additional parameters to pass to BiG-SCAPE.
+        version: The version of BiG-SCAPE to run. Can be 1 or 2.
+
+    Returns:
+        True if BiG-SCAPE ran successfully, False otherwise.
     """
     # switch to correct version of BiG-SCAPE
     if version == 1:
