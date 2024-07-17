18b231786aacc6210ac2cd144a82e6e8481d0229
Arjan Draisma <arjan.draisma@wur.nl>
2024-07-17 11:34:55 +0200

    use tmp path instead of data path

--- tests/unit/genomics/test_runbigscape.py
+++ tests/unit/genomics/test_runbigscape.py
@@ -6,7 +6,7 @@ from .. import DATA_DIR
 def test_run_bigscape_v1(tmp_path):
     """Test whether BiG-SCAPE v1 runs at all using the --help command"""
     result = bigscape.run_bigscape(
-        antismash_path=DATA_DIR,
+        antismash_path=tmp_path,
         output_path=tmp_path,
         extra_params="--help",
         version=1,
@@ -18,7 +18,7 @@ def test_run_bigscape_v1(tmp_path):
 def test_run_bigscape_v2(tmp_path):
     """Test whether BiG-SCAPE v2 runs at all using the --help command"""
     result = bigscape.run_bigscape(
-        antismash_path=DATA_DIR,
+        antismash_path=tmp_path,
         output_path=tmp_path,
         extra_params="--help",
         version=2,
