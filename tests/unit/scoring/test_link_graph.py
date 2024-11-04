d4bf9fb2d277424faa52181db9750a85d000c322
Annette Lien <a.lien@posteo.de>
2024-11-04 21:32:05 +0100

    tests: adapted the test to the changes

--- tests/unit/scoring/test_link_graph.py
+++ tests/unit/scoring/test_link_graph.py
@@ -116,10 +116,8 @@ def test_filter(gcfs, spectra, score):
 
 def test_link_to_dict(lg, gcfs, spectra, score):
     link = lg.links[0]
-    index = 1
-    dict_repr = lg.link_to_dict(link, index)
+    dict_repr = lg.link_to_dict(link)
     assert type(dict_repr) is dict
-    assert dict_repr["index"] == 1
     assert dict_repr["genomic_object_type"] == gcfs[0].__class__.__name__
     assert dict_repr["genomic_object_id"] == gcfs[0].id
     assert dict_repr["metabolomic_object_type"] == spectra[0].__class__.__name__
@@ -128,15 +126,17 @@ def test_link_to_dict(lg, gcfs, spectra, score):
     assert dict_repr["rosetta_score"] == ""
 
 
-def test_get_table_data(lg, gcfs, spectra, score):
+def test__links_to_dicts(lg, gcfs, spectra, score):
     # add a second link
     lg.add_link(gcfs[1], spectra[1], metcalf=score)
 
-    table_data = lg.get_table_data()
+    table_data = lg._links_to_dicts()
     assert type(table_data) is list
     assert type(table_data[0]) is dict
     assert len(table_data) == 2
+    assert table_data[0]["index"] == 1
+    assert table_data[1]["index"] == 2
 
     display_limit = 1
-    table_data = lg.get_table_data(display_limit)
+    table_data = lg._links_to_dicts(display_limit)
     assert len(table_data) == 1
