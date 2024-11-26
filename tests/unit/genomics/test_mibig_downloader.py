3fab02cf399678f7c71ba1de56934b879afa21c1
Cunliang Geng <c.geng@esciencecenter.nl>
2024-11-26 14:28:59 +0100

    add mibig v4.0 to mibig_downloader

--- tests/unit/genomics/test_mibig_downloader.py
+++ tests/unit/genomics/test_mibig_downloader.py
@@ -3,26 +3,21 @@ from nplinker.genomics import mibig
 
 
 class TestDownloadAndExtractMibigMetadata:
-    def test_default(self, tmp_path):
+    @pytest.mark.parametrize(
+        "version, expected",
+        [
+            ["1.4", "mibig_json_1.4.tar.gz"],
+            ["3.1", "mibig_json_3.1.tar.gz"],
+            ["4.0", "mibig_json_4.0.tar.gz"],
+        ],
+    )
+    def test_version(self, tmp_path, version, expected):
         download_path = tmp_path / "download"
         extract_path = tmp_path / "metadata"
         download_path.mkdir()
         extract_path.mkdir()
-        mibig.download_and_extract_mibig_metadata(download_path, extract_path)
-        archive = download_path / "mibig_json_3.1.tar.gz"
-        metadata = extract_path / "BGC0000002.json"
-        assert archive.exists()
-        assert archive.is_file()
-        assert metadata.exists()
-        assert metadata.is_file()
-
-    def test_version(self, tmp_path):
-        download_path = tmp_path / "download"
-        extract_path = tmp_path / "metadata"
-        download_path.mkdir()
-        extract_path.mkdir()
-        mibig.download_and_extract_mibig_metadata(download_path, extract_path, version="1.4")
-        archive = download_path / "mibig_json_1.4.tar.gz"
+        mibig.download_and_extract_mibig_metadata(download_path, extract_path, version=version)
+        archive = download_path / expected
         metadata = extract_path / "BGC0000002.json"
         assert archive.exists()
         assert archive.is_file()
