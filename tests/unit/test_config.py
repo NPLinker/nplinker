a5317f58c478ed0212a822405f7b5fc36fb9b329
Cunliang Geng <c.geng@esciencecenter.nl>
2024-11-27 15:01:32 +0100

    update unit tests

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,7 +4,7 @@ from . import CONFIG_FILE_LOCAL_MODE
 
 
 def test_config(tmp_path):
-    """Test loading the default config file."""
+    """Test loading config file."""
     os.environ["NPLINKER_ROOT_DIR"] = str(tmp_path)  # Create a temporary root dir for NPLinker
     config = load_config(CONFIG_FILE_LOCAL_MODE)
 
@@ -13,17 +13,12 @@ def test_config(tmp_path):
     assert config["log.level"] == "DEBUG"
     assert config.get("log.level") == "DEBUG"
 
-    # The following are default values from nplinker_default.toml
     assert config.get("log.file") is None
     assert config.log.use_console is True
 
     assert config.mibig.to_use is True
     assert config.mibig.version == "3.1"
 
-    assert (
-        config.bigscape.parameters
-        == "--mibig --clans-off --mix --include_singletons --cutoffs 0.30"
-    )
     assert config.bigscape.cutoff == "0.30"
     assert config.bigscape.version == 1
 
