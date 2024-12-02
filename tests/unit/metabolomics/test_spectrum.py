7cd675f9318eb44ff337acc277250983f035d33d
Annette Lien <a.lien@posteo.de>
2024-12-02 18:32:30 +0100

    test: trying to fix unit test issue where the spectrum rt is a dict instead of numerical

--- tests/unit/metabolomics/test_spectrum.py
+++ tests/unit/metabolomics/test_spectrum.py
@@ -72,7 +72,14 @@ def test_has_strain():
 
 def test_to_dict():
     """Test the to_dict method."""
-    spec = Spectrum("spec1", [100, 200], [0.1, 0.2], 150, 0, {"info": "test"})
+    spec = Spectrum(
+        id="spec1",
+        mz=[100.0, 200.0],
+        intensity=[0.1, 0.2],
+        precursor_mz=150.0,
+        rt=0.0,
+        metadata={"info": "test"},
+    )
     spec.strains.add(Strain("strain1"))
     spec.strains.add(Strain("strain2"))
 
