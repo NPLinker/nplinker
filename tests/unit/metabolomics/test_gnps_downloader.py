668bc789026e37a281e541d2e660045c87434e88
Cunliang Geng <c.geng@esciencecenter.nl>
2025-01-08 14:45:12 +0100

    remove tests for property gnps_format

--- tests/unit/metabolomics/test_gnps_downloader.py
+++ tests/unit/metabolomics/test_gnps_downloader.py
@@ -18,19 +18,6 @@ def test_unknown_workflow(tmpdir):
 
 
 @pytest.mark.parametrize(
-    "task_id, expected",
-    [
-        ["92036537c21b44c29e509291e53f6382", GNPSFormat.FBMN],
-        ["c22f44b14a3d450eb836d607cb9521bb", GNPSFormat.SNETS],
-        ["189e8bf16af145758b0a900f1c44ff4a", GNPSFormat.SNETSV2],
-    ],
-)
-def test_supported_workflows(task_id, expected, tmpdir):
-    downloader = GNPSDownloader(task_id, tmpdir)
-    assert downloader.gnps_format == expected
-
-
-@pytest.mark.parametrize(
     "task_id, filename",
     [
         [
