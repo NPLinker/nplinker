4435369c4803404158e577ee92b97819b564e2bf
Cunliang Geng <c.geng@esciencecenter.nl>
2025-01-10 14:27:23 +0100

    add tests for gnps2 data

--- tests/unit/metabolomics/test_gnps_extractor.py
+++ tests/unit/metabolomics/test_gnps_extractor.py
@@ -39,3 +39,30 @@ def test_extract_gnps1(
     assert mf_file.exists()
     assert annotations_file.exists()
 
+
+#
+# Tests for GNPS2 data
+#
+
+
+def test_unknown_workflow_gnps2(gnps2_tar_files, tmpdir):
+    with pytest.raises(ValueError, match="Unknown workflow type for GNPS archive .*"):
+        GNPSExtractor(gnps2_tar_files[GNPSFormat.Unknown], tmpdir)
+
+
+@pytest.mark.parametrize("workflow", [GNPSFormat.GNPS2CN, GNPSFormat.GNPS2FBMN])
+def test_extract_gnps2(workflow, gnps2_tar_files, tmpdir):
+    extractor = GNPSExtractor(gnps2_tar_files[workflow], tmpdir)
+    assert extractor.gnps_format == workflow
+    assert extractor.extract_dir == tmpdir
+    assert len(list(Path(tmpdir).iterdir())) == 4
+
+    file_mappings_file = tmpdir / "file_mappings.csv"
+    spec_file = tmpdir / "spectra.mgf"
+    mf_file = tmpdir / "molecular_families.tsv"
+    annotations_file = tmpdir / "annotations.tsv"
+
+    assert file_mappings_file.exists()
+    assert spec_file.exists()
+    assert mf_file.exists()
+    assert annotations_file.exists()
