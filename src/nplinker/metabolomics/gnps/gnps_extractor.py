1c10b9af44ee4718917471f3fc00f0697619b801
Cunliang Geng <c.geng@esciencecenter.nl>
2025-01-20 16:58:03 +0100

    update GNPSExtractor class to support gnps2 (#295)
    Updated GNPSExtractor class to support gnps2 data.

--- src/nplinker/metabolomics/gnps/gnps_extractor.py
+++ src/nplinker/metabolomics/gnps/gnps_extractor.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import os
+import tarfile
 import zipfile
 from os import PathLike
 from pathlib import Path
@@ -9,7 +10,7 @@ from .gnps_format import gnps_format_from_archive
 
 
 class GNPSExtractor:
-    """Extract files from a GNPS molecular networking archive (.zip).
+    """Extract files from a GNPS molecular networking archive.
 
     ??? info "Concept"
         [GNPS data][gnps-data]
@@ -39,6 +40,16 @@ class GNPSExtractor:
         - spectra/*.mgf
         - networkedges_selfloop/*.selfloop
         - DB_result/*.tsv
+    4. GNPS2 classical_networking_workflow
+        - nf_output/clustering/featuretable_reformatted_presence.csv
+        - nf_output/clustering/specs_ms.mgf
+        - nf_output/networking/filtered_pairs.tsv
+        - nf_output/library/merged_results_with_gnps.tsv
+    5. GNPS2 feature_based_molecular_networking_workflow
+        - nf_output/clustering/featuretable_reformated.csv
+        - nf_output/clustering/specs_ms.mgf
+        - nf_output/networking/filtered_pairs.tsv
+        - nf_output/library/merged_results_with_gnps.tsv
 
     Attributes:
         gnps_format: The GNPS workflow type.
@@ -49,7 +60,7 @@ class GNPSExtractor:
         """Initialize the GNPSExtractor.
 
         Args:
-            file: The path to the GNPS zip file.
+            file: The path to the GNPS archive file.
             extract_dir: path to the directory where to extract the files to.
 
         Raises:
@@ -64,12 +75,7 @@ class GNPSExtractor:
         """
         gnps_format = gnps_format_from_archive(file)
         if gnps_format == GNPSFormat.Unknown:
-            raise ValueError(
-                f"Unknown workflow type for GNPS archive '{file}'."
-                f"Supported GNPS workflows are described in the GNPSFormat enum, "
-                f"including such as 'METABOLOMICS-SNETS', 'METABOLOMICS-SNETS-V2' "
-                f"and 'FEATURE-BASED-MOLECULAR-NETWORKING'."
-            )
+            raise ValueError(f"Unknown workflow type for GNPS archive '{file}'.")
 
         self._file = Path(file)
         self._extract_path = Path(extract_dir)
@@ -104,22 +110,29 @@ class GNPSExtractor:
 
     def _extract(self):
         """Extract required files from archive."""
-        if self._gnps_format == GNPSFormat.SNETS:
-            self._extract_snets()
-        elif self._gnps_format == GNPSFormat.SNETSV2:
-            self._extract_snetsv2()
-        elif self._gnps_format == GNPSFormat.FBMN:
-            self._extract_fbmn()
+        extract_methods = {
+            GNPSFormat.SNETS: self._extract_snets,
+            GNPSFormat.SNETSV2: self._extract_snetsv2,
+            GNPSFormat.FBMN: self._extract_fbmn,
+            GNPSFormat.GNPS2CN: self._extract_gnps2cn,
+            GNPSFormat.GNPS2FBMN: self._extract_gnps2fbmn,
+        }
+
+        extract_method = extract_methods.get(self._gnps_format)
+        if extract_method:
+            extract_method()
+        else:
+            raise ValueError(f"Unsupported GNPS format: {self._gnps_format}")
 
     def _extract_snets(self):
         # the order of members matters
         members = [
-            self._select_member(
+            self._select_zip_member(
                 "clusterinfosummarygroup_attributes_withIDs_withcomponentID", ".tsv"
             ),
-            self._select_member("METABOLOMICS-SNETS", ".mgf"),
-            self._select_member("networkedges_selfloop", ".pairsinfo"),
-            self._select_member("result_specnets_DB", ".tsv"),
+            self._select_zip_member("METABOLOMICS-SNETS", ".mgf"),
+            self._select_zip_member("networkedges_selfloop", ".pairsinfo"),
+            self._select_zip_member("result_specnets_DB", ".tsv"),
         ]
         utils.extract_archive(self._file, self._extract_path, members)
         # rename the files to the expected names
@@ -133,12 +146,12 @@ class GNPSExtractor:
     def _extract_snetsv2(self):
         # the order of members matters
         members = [
-            self._select_member(
+            self._select_zip_member(
                 "clusterinfosummarygroup_attributes_withIDs_withcomponentID", ".clustersummary"
             ),
-            self._select_member("METABOLOMICS-SNETS-V2", ".mgf"),
-            self._select_member("networkedges_selfloop", ".selfloop"),
-            self._select_member("result_specnets_DB", ".tsv"),
+            self._select_zip_member("METABOLOMICS-SNETS-V2", ".mgf"),
+            self._select_zip_member("networkedges_selfloop", ".selfloop"),
+            self._select_zip_member("result_specnets_DB", ".tsv"),
         ]
         utils.extract_archive(self._file, self._extract_path, members)
         os.renames(
@@ -151,18 +164,18 @@ class GNPSExtractor:
         # there might be two folders for quantification table
         # "quantification_table_reformatted" and "quantification_table"
         try:
-            quantification_table_member = self._select_member(
+            quantification_table_member = self._select_zip_member(
                 "quantification_table_reformatted", ".csv"
             )
         except ValueError:
-            quantification_table_member = self._select_member("quantification_table", ".csv")
+            quantification_table_member = self._select_zip_member("quantification_table", ".csv")
 
         # the order of members matters
         members = [
             quantification_table_member,
-            self._select_member("spectra", ".mgf"),
-            self._select_member("networkedges_selfloop", ".selfloop"),
-            self._select_member("DB_result", ".tsv"),
+            self._select_zip_member("spectra", ".mgf"),
+            self._select_zip_member("networkedges_selfloop", ".selfloop"),
+            self._select_zip_member("DB_result", ".tsv"),
         ]
         utils.extract_archive(self._file, self._extract_path, members)
         os.renames(
@@ -171,12 +184,59 @@ class GNPSExtractor:
         for member, fname in zip(members[1:], self._target_files[1:]):
             os.renames(self._extract_path / member, self._extract_path / fname)
 
-    def _select_member(self, prefix: str, suffix: str) -> str:
-        """Helper function to extract files matching a prefix and suffix from the archive."""
+    def _extract_gnps2cn(self):
+        # the order of members matters
+        members = [
+            self._select_tar_member(
+                "nf_output/clustering/featuretable_reformatted_presence", ".csv"
+            ),
+            self._select_tar_member("nf_output/clustering/specs_ms", ".mgf"),
+            self._select_tar_member("nf_output/networking/filtered_pairs", ".tsv"),
+            self._select_tar_member("nf_output/library/merged_results_with_gnps", ".tsv"),
+        ]
+        utils.extract_archive(self._file, self._extract_path, members)
+        os.renames(
+            self._extract_path / members[0], self._extract_path / (self._target_files[0] + ".csv")
+        )
+        for member, fname in zip(members[1:], self._target_files[1:]):
+            os.renames(self._extract_path / member, self._extract_path / fname)
+
+    def _extract_gnps2fbmn(self):
+        # the order of members matters
+        members = [
+            self._select_tar_member("nf_output/clustering/featuretable_reformated", ".csv"),
+            self._select_tar_member("nf_output/clustering/specs_ms", ".mgf"),
+            self._select_tar_member("nf_output/networking/filtered_pairs", ".tsv"),
+            self._select_tar_member("nf_output/library/merged_results_with_gnps", ".tsv"),
+        ]
+        utils.extract_archive(self._file, self._extract_path, members)
+        os.renames(
+            self._extract_path / members[0], self._extract_path / (self._target_files[0] + ".csv")
+        )
+        for member, fname in zip(members[1:], self._target_files[1:]):
+            os.renames(self._extract_path / member, self._extract_path / fname)
+
+    def _select_zip_member(self, prefix: str, suffix: str) -> str:
+        """Helper function to extract files matching a prefix and suffix from the zip archive."""
         with zipfile.ZipFile(self._file) as zf:
             member_list = [
                 member
-                for member in zf.namelist()
+                for member in zf.namelist()  # relative path
+                if member.startswith(prefix) and member.endswith(suffix)
+            ]
+            if len(member_list) != 1:
+                raise ValueError(
+                    f"Expected exactly one file matching pattern '{prefix}*{suffix}'"
+                    f"in archive '{self._file}', but found {len(member_list)}."
+                )
+        return member_list[0]
+
+    def _select_tar_member(self, prefix: str, suffix: str) -> str:
+        """Helper function to extract files matching a prefix and suffix from the tar archive."""
+        with tarfile.open(self._file) as tf:
+            member_list = [
+                member
+                for member in tf.getnames()
                 if member.startswith(prefix) and member.endswith(suffix)
             ]
             if len(member_list) != 1:
--- tests/unit/metabolomics/test_gnps_extractor.py
+++ tests/unit/metabolomics/test_gnps_extractor.py
@@ -1,33 +1,28 @@
-import filecmp
 from pathlib import Path
 import pytest
 from nplinker.metabolomics.gnps import GNPSExtractor
 from nplinker.metabolomics.gnps import GNPSFormat
 
 
-def test_unknown_workflow(gnps_zip_files, tmpdir):
+#
+# Tests for GNPS1 data
+#
+
+
+def test_unknown_workflow_gnps1(gnps_zip_files, tmpdir):
     with pytest.raises(ValueError, match="Unknown workflow type for GNPS archive .*"):
         GNPSExtractor(gnps_zip_files[GNPSFormat.Unknown], tmpdir)
 
 
 @pytest.mark.parametrize("workflow", [GNPSFormat.FBMN, GNPSFormat.SNETS, GNPSFormat.SNETSV2])
-def test_supported_workflows(workflow, gnps_zip_files, tmpdir):
+def test_extract_gnps1(
+    workflow,
+    gnps_zip_files,
+    tmpdir,
+):
     extractor = GNPSExtractor(gnps_zip_files[workflow], tmpdir)
     assert extractor.gnps_format == workflow
     assert extractor.extract_dir == tmpdir
-
-
-@pytest.mark.parametrize("workflow", [GNPSFormat.FBMN, GNPSFormat.SNETS, GNPSFormat.SNETSV2])
-def test_extract(
-    workflow,
-    gnps_zip_files,
-    gnps_file_mappings_files,
-    gnps_spectra_files,
-    gnps_mf_files,
-    gnps_annotations_files,
-    tmpdir,
-):
-    GNPSExtractor(gnps_zip_files[workflow], tmpdir)
     assert len(list(Path(tmpdir).iterdir())) == 4
 
     file_mappings_file = (
@@ -44,7 +39,30 @@ def test_extract(
     assert mf_file.exists()
     assert annotations_file.exists()
 
-    assert filecmp.cmp(file_mappings_file, gnps_file_mappings_files[workflow])
-    assert filecmp.cmp(spec_file, gnps_spectra_files[workflow])
-    assert filecmp.cmp(mf_file, gnps_mf_files[workflow])
-    assert filecmp.cmp(annotations_file, gnps_annotations_files[workflow])
+
+#
+# Tests for GNPS2 data
+#
+
+
+def test_unknown_workflow_gnps2(gnps2_tar_files, tmpdir):
+    with pytest.raises(ValueError, match="Unknown workflow type for GNPS archive .*"):
+        GNPSExtractor(gnps2_tar_files[GNPSFormat.Unknown], tmpdir)
+
+
+@pytest.mark.parametrize("workflow", [GNPSFormat.GNPS2CN, GNPSFormat.GNPS2FBMN])
+def test_extract_gnps2(workflow, gnps2_tar_files, tmpdir):
+    extractor = GNPSExtractor(gnps2_tar_files[workflow], tmpdir)
+    assert extractor.gnps_format == workflow
+    assert extractor.extract_dir == tmpdir
+    assert len(list(Path(tmpdir).iterdir())) == 4
+
+    file_mappings_file = tmpdir / "file_mappings.csv"
+    spec_file = tmpdir / "spectra.mgf"
+    mf_file = tmpdir / "molecular_families.tsv"
+    annotations_file = tmpdir / "annotations.tsv"
+
+    assert file_mappings_file.exists()
+    assert spec_file.exists()
+    assert mf_file.exists()
+    assert annotations_file.exists()
