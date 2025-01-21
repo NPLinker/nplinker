b9524c69a8b63513bc833c901d431fa355c6f0c6
Cunliang Geng <c.geng@esciencecenter.nl>
2025-01-21 16:30:47 +0100

    update GNPSSpectrumLoader for gnps2 (#299)

--- tests/unit/metabolomics/test_gnps_spectrum_loader.py
+++ tests/unit/metabolomics/test_gnps_spectrum_loader.py
@@ -7,6 +7,15 @@ from nplinker.metabolomics.gnps import GNPSSpectrumLoader
     "workflow, num_spectra",
     [[GNPSFormat.FBMN, 1492], [GNPSFormat.SNETS, 25935], [GNPSFormat.SNETSV2, 7383]],
 )
-def test_gnps_spectrum_loader(workflow, num_spectra, gnps_spectra_files):
+def test_gnps_spectrum_loader_gnps1(workflow, num_spectra, gnps_spectra_files):
     loader = GNPSSpectrumLoader(gnps_spectra_files[workflow])
+    assert len(loader.spectra) == num_spectra
+
+
+@pytest.mark.parametrize(
+    "workflow, num_spectra",
+    [[GNPSFormat.GNPS2CN, 1051], [GNPSFormat.GNPS2FBMN, 371]],
+)
+def test_gnps_spectrum_loader_gnps2(workflow, num_spectra, gnps2_spectra_files):
+    loader = GNPSSpectrumLoader(gnps2_spectra_files[workflow])
     assert len(loader.spectra) == num_spectra
