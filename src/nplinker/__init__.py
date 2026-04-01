1a4603b9171b1a8e1d791cfc4b358bf51f3cfb87
CunliangGeng <CunliangGeng@users.noreply.github.com>
2026-04-01 09:39:31 +0000

    Update version to 2.0.3

--- src/nplinker/__init__.py
+++ src/nplinker/__init__.py
@@ -7,7 +7,7 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Cunliang Geng"
 __email__ = "c.geng@esciencecenter.nl"
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 
 
 __all__ = ["NPLinker", "setup_logging"]
